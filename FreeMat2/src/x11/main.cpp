a1d3b31b860a8a83acb99ef26e0d9498c814c7cc
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-01-18 18:15:48 +0000

    Added X11 files to their own subdir...
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@83 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat2/src/x11/main.cpp
@@ -0,0 +1,123 @@
+#include <X11/Xlib.h>
+#include <unistd.h>
+#include "XWindow.hpp"
+#include "BitmapFont.hpp"
+#include "Terminal.hpp"
+#include <stdio.h>
+#include "Module.hpp"
+#include "LoadCore.hpp"
+#include "GraphicsCore.hpp"
+#include "System.hpp"
+#include "PathSearch.hpp"
+#include <stdlib.h>
+#include <signal.h>
+
+
+using namespace FreeMat;
+
+Display *d;
+int screen_num = 0;
+
+namespace FreeMat {
+  Context *context;
+}
+
+Terminal term;
+
+sig_t signal_suspend_default;
+sig_t signal_resume_default;
+
+void signal_suspend(int a) {
+  term.RestoreOriginalMode();
+  printf("Suspending FreeMat...\n");
+  fflush(stdout);
+  signal(SIGTSTP,signal_suspend_default);
+  raise(SIGTSTP);
+}
+
+void signal_resume(int a) {
+  fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+  printf("Resuming FreeMat...\n");
+  term.SetRawMode();
+  term.Redisplay();
+}
+
+void signal_resize(int a) {
+  printf("Resize...\n");
+  term.ResizeEvent();
+}
+
+void stdincb() {
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1) {
+    term.ProcessChar(c);
+  }
+}
+
+std::string GetApplicationPath(char *argv0) {
+  std::string retpath;
+  // Case 1 - absolute path
+  if (argv0[0] == '/') 
+    return GetPathOnly(std::string(argv0));
+  // Case 2 - relative path
+  char buffer[4096];
+  getcwd(buffer,sizeof(buffer));
+  retpath = CheckEndSlash(std::string(buffer)) + std::string(argv0);
+  // This file should exist
+  if (FileExists(retpath))
+    return GetPathOnly(std::string(retpath));
+  printf("Searching PATH...\n");
+  PathSearcher psearch("PATH");
+  return GetPathOnly(psearch.ResolvePath(argv0));
+}
+
+int main(int argc, char *argv[]) {
+  std::string paths;
+
+  paths = GetApplicationPath(argv[0]);
+  XPSetFontPath(paths);
+  
+  signal_suspend_default = signal(SIGTSTP,signal_suspend);
+  signal_resume_default = signal(SIGCONT,signal_resume);
+  signal(SIGWINCH, signal_resize);
+  d = XOpenDisplay(0);
+  if (!d) {
+    fprintf(stderr,"Error - unable to open X display\n");
+    exit(1);
+  }
+  fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL) | O_NONBLOCK);
+  SetActiveDisplay(d);
+  RegisterSTDINCallback(stdincb);
+  context = new Context;
+  BuiltInFunctionDef *f2def = new BuiltInFunctionDef;
+  f2def->retCount = 0;
+  f2def->argCount = 5;
+  f2def->name = "loadFunction";
+  f2def->fptr = LoadFunction;
+  context->insertFunctionGlobally(f2def);
+  
+  SpecialFunctionDef *sfdef = new SpecialFunctionDef;
+  sfdef->retCount = 0;
+  sfdef->argCount = 5;
+  sfdef->name = "import";
+  sfdef->fptr = ImportFunction;
+  context->insertFunctionGlobally(sfdef);
+
+  LoadCoreFunctions(context);
+  LoadGraphicsCoreFunctions(context);  
+  InitializePlotSubsystem();
+  InitializeImageSubsystem();
+  const char *envPtr;
+  envPtr = getenv("FREEMAT_PATH");
+  if (envPtr)
+    term.initialize(std::string(envPtr),context);
+  else
+    term.initialize(std::string(""),context);
+  WalkTree *twalk = new WalkTree(context,&term);
+  term.SetEvalEngine(twalk);
+  term.outputMessage(" Freemat - build ");
+  term.outputMessage(__DATE__);
+  term.outputMessage("\n");
+  term.outputMessage(" Copyright (c) 2002-2004 by Samit Basu\n");
+  twalk->evalCLI();
+}

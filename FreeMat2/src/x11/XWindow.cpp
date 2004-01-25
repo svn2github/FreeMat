4a6b2aa5bb0b008994e42247b7e54a3d9d2441c4
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-01-25 18:11:53 +0000

    XWindow modified to use native drawing.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@93 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/src/x11/XWindow.cpp
+++ FreeMat2/src/x11/XWindow.cpp
@@ -39,8 +39,9 @@ void UnregisterXWindow(XWindow *p) {
   winlist.erase(p->getWindow());
 }
 
-XWindow::XWindow() {
+XWindow::XWindow(WindowType wtype) {
   m_display = theDisplay;
+  m_type = wtype;
   m_window = XCreateSimpleWindow(m_display, RootWindow(m_display, 0),
 				 0, 0, 500, 400, 0, 
 				 BlackPixel(m_display, 0),
@@ -62,6 +63,8 @@ XWindow::XWindow() {
   if ((proto_atom != None) && (delete_atom != None))
     XChangeProperty(m_display, m_window, proto_atom, XA_ATOM, 32,
 		    PropModeReplace, (unsigned char *)&delete_atom, 1);
+  current_fontname = "none";
+  current_fontsize = 0;
 }
 
 void XWindow::Raise() {
@@ -96,9 +99,11 @@ XWindow::~XWindow() {
 }
 
 void XWindow::OnExpose(int x, int y, int w, int h) {
-  if (m_pixmap != None)
+  if (m_type == BitmapWindow) 
     XCopyArea(m_display, m_pixmap, m_window, m_gc,
-	      x, y, w, h, x, y);
+  	      x, y, w, h, x, y);
+  else
+    OnDraw(*this);
 }
 
 void XWindow::Refresh() {
@@ -189,12 +194,14 @@ void XWindow::OnResize(int w, int h) {
   if (w == 0 || h == 0) return;
   m_width = w;
   m_height = h;
-  // Update the contents.
-  unsigned char *data;
-  data = (unsigned char *) malloc(3*sizeof(char)*w*h);
-  UpdateContents(data,w,h);
-  SetImage(data,w,h);
-  free(data);
+  if (m_type == BitmapWindow)
+    OnDraw(*this);
+//   // Update the contents.
+//   unsigned char *data;
+//   data = (unsigned char *) malloc(3*sizeof(char)*w*h);
+//   UpdateContents(data,w,h);
+//   SetImage(data,w,h);
+//   free(data);
   OnSize();
   XClearArea(m_display, m_window, 0, 0, w, h, True);
   XFlush(m_display);
@@ -273,10 +280,11 @@ void XWindow::SetImagePseudoColor(unsigned char *data, int width, int height) {
 }
 
 void XWindow::UpdateContents(unsigned char *data, int width, int height) {
-  RGBImage img(width, height, data);
-  RGBImageGC gc(img);
-  img.SetAllPixels(Color("light grey"));
-  OnDraw(gc);
+  OnDraw(*this);
+//   RGBImage img(width, height, data);
+//   RGBImageGC gc(img);
+//   img.SetAllPixels(Color("light grey"));
+//   OnDraw(gc);
 }
 
 void XWindow::Print(std::string filename) {
@@ -403,6 +411,212 @@ void XWindow::GetBox(int &x1, int &y1, int &x2, int &y2) {
   XUndefineCursor(m_display, m_window);
 }
 
+Point2D XWindow::GetCanvasSize() {
+  return Point2D(m_width,m_height);
+}
+
+Point2D XWindow::GetTextExtent(std::string label) {
+  return Point2D(XTextWidth(font_info, label.c_str(), label.size()),
+		 font_info->ascent + font_info->descent);
+}
+
+void XWindow::DrawTextString(std::string label, Point2D pos, OrientationType orient) {
+  if (orient == ORIENT_0) {
+    XDrawString(m_display, m_window, m_gc, pos.x, pos.y, 
+		label.c_str(), label.size());
+    return;
+  }
+  if (orient == ORIENT_90) {
+    // Get the size of the string
+    Point2D stringSize(GetTextExtent(label));
+    // Create a bitmap of this size
+    Pixmap bitmap, rotbitmap;
+    bitmap = XCreatePixmap(m_display, m_window, stringSize.x, 
+			   stringSize.y, 1);
+    rotbitmap = XCreatePixmap(m_display, m_window, stringSize.y, 
+			   stringSize.x, 1);
+    if (!bitmap) throw 
+      FreeMat::Exception("Unable to create bitmap for rotated text!\n");
+    GC fontgc;
+    fontgc = XCreateGC(m_display, bitmap, 0, NULL);
+    XSetForeground(m_display, fontgc, WhitePixel(m_display, 0));
+    XFillRectangle(m_display, bitmap, fontgc, 0, 0, 
+		   stringSize.x, stringSize.y);
+    XSetForeground(m_display, fontgc, BlackPixel(m_display, 0));
+    XDrawString(m_display, bitmap, fontgc, font_info->max_bounds.lbearing, 
+		font_info->ascent, label.c_str(), label.size());
+    XImage *I1;
+    I1 = XGetImage(m_display, bitmap, 0, 0, stringSize.x, 
+		   stringSize.y, 1, XYPixmap);
+    I1->format = XYBitmap;
+    int rotw, roth;
+    rotw = stringSize.y;
+    roth = stringSize.x;
+    int bytew;
+    bytew = (rotw-1)/8 + 1;
+    unsigned char *bitdata2;
+    bitdata2=(unsigned char *) malloc((unsigned)(bytew * roth));
+    XImage *I2;
+    I2 = XCreateImage(m_display, DefaultVisual(m_display, 0),
+		      1, XYBitmap, 0, (char*) bitdata2, rotw, roth, 8, 0);
+    memset(bitdata2, 255, bytew*roth);
+    for (int j=0;j<rotw;j++)
+      for (int i=0;i<roth;i++) 
+	XPutPixel(I2, j, roth-i-1, XGetPixel(I1, i, j));
+    XSetForeground(m_display, fontgc, WhitePixel(m_display, 0));
+    XFillRectangle(m_display, rotbitmap, fontgc, 0, 0, roth, rotw);
+    XSetForeground(m_display, fontgc, BlackPixel(m_display, 0));
+    XPutImage(m_display, rotbitmap, fontgc, I2, 0, 0, 0, 0, rotw, roth);
+    XSetStipple(m_display, m_gc, rotbitmap);
+    XSetFillStyle(m_display, m_gc, FillStippled);
+    int newx, newy;
+    newx = pos.x - rotw;
+    newy = pos.y - roth;
+    XSetTSOrigin(m_display, m_gc, newx, newy);
+    XFillRectangle(m_display, m_window, m_gc, newx, newy, rotw, roth);
+    XSetFillStyle(m_display, m_gc, FillSolid);
+    XDestroyImage(I1);
+    XDestroyImage(I2);
+    XFreePixmap(m_display,bitmap);
+    XFreePixmap(m_display,rotbitmap);
+    XFreeGC(m_display,fontgc);
+  }
+}
+
+void XWindow::SetFont(std::string fontname, int fontsize) {
+  if ((fontname == current_fontname) && (fontsize == current_fontsize))
+    return;
+  char buffer[1000];
+  sprintf(buffer,"-adobe-helvetica-medium-r-normal--%d-*",fontsize);
+  //   sprintf(buffer,"-adobe-helvetica-medium-r-normal--[%f %f %f %f]-*",
+  // 	  0,fontsize/10.0,fontsize/10.0,0);
+  //sprintf(buffer,"-*-helvetica-*-%d-*",fontsize);
+  int cnt;
+  char **flist = XListFonts(m_display, buffer, 1000, &cnt);
+  if (cnt == 0) {
+    printf("No match on font name\r\n");
+    return;
+  }
+  font_info = XLoadQueryFont(m_display, flist[0]);
+  XSetFont(m_display, m_gc, font_info->fid);
+  XFreeFontNames(flist);
+}
+
+Color XWindow::SetBackGroundColor(Color col) {
+  Color retsave(bg);
+  bg = col;
+  XColor t;
+  t.red = col.red * 257;
+  t.green = col.green * 257;
+  t.blue = col.blue * 257;
+  XAllocColor(m_display, DefaultColormap(m_display, 0), &t);
+  XSetBackground(m_display, m_gc, t.pixel);
+  return retsave;
+}
+
+Color XWindow::SetForeGroundColor(Color col) {
+  Color retsave(fg);
+  fg = col;
+  XColor t;
+  t.red = col.red * 257;
+  t.green = col.green * 257;
+  t.blue = col.blue * 257;
+  XAllocColor(m_display, DefaultColormap(m_display, 0), &t);
+  XSetForeground(m_display, m_gc, t.pixel);
+  return retsave;
+}
+
+LineStyleType XWindow::SetLineStyle(LineStyleType style) {
+  unsigned char line_dashed_list[2] = {4, 4};
+  unsigned char line_dotted_list[2] = {3, 1};
+  unsigned char line_dash_dot_list[4] = {3, 4, 3, 1};
+  LineStyleType retsav(ls);
+  ls = style;
+  switch (ls) {
+  case LINE_SOLID:
+    XSetLineAttributes(m_display, m_gc, 1, LineSolid, CapButt, JoinMiter);
+    break;
+  case LINE_DASHED:
+    XSetDashes(m_display, m_gc, 0, (const char*) line_dashed_list, 2);
+    break;
+  case LINE_DOTTED:
+    XSetDashes(m_display, m_gc, 0, (const char*) line_dotted_list, 2);
+    break;
+  case LINE_DASH_DOT:
+    XSetDashes(m_display, m_gc, 0, (const char*) line_dash_dot_list, 4);
+    break;
+  }
+  return retsav;
+}
+
+void XWindow::DrawLine(Point2D pos1, Point2D pos2) {
+  XDrawLine(m_display, m_window, m_gc, pos1.x, pos1.y, pos2.x, pos2.y);
+}
+
+void XWindow::DrawPoint(Point2D pos) {
+  XDrawPoint(m_display, m_window, m_gc, pos.x, pos.y);
+}
+
+void XWindow::DrawCircle(Point2D pos, int radius) {
+  XDrawArc(m_display, m_window, m_gc, pos.x - radius, pos.y - radius, radius*2, 
+	   radius*2 , 0, 64*360);
+}
+
+void XWindow::DrawRectangle(Rect2D rect) {
+  XDrawRectangle(m_display, m_window, m_gc, rect.x1, rect.y1, rect.width, rect.height);
+}
+
+void XWindow::FillRectangle(Rect2D rect) {
+  XFillRectangle(m_display, m_window, m_gc, rect.x1, rect.y1, rect.width, rect.height);
+}
+
+void XWindow::DrawLines(std::vector<Point2D> pts) {
+  XPoint *t;
+  t = (XPoint*) malloc(sizeof(XPoint)*pts.size());
+  int i;
+  for (i=0;i<pts.size();i++) {
+    t[i].x = pts[i].x;
+    t[i].y = pts[i].y;
+  }
+  XDrawLines(m_display, m_window, m_gc, t, pts.size(), CoordModeOrigin);
+  free(t);
+}
+
+void XWindow::PushClippingRegion(Rect2D rect) {
+  XRectangle clipwin;
+  clipwin.x = rect.x1;
+  clipwin.y = rect.y1;
+  clipwin.width = rect.width;
+  clipwin.height = rect.height;
+  XSetClipRectangles(m_display, m_gc, 0, 0, &clipwin, 1, Unsorted);
+  clipstack.push_back(rect);
+}
+
+Rect2D XWindow::PopClippingRegion() {
+  clipstack.pop_back();
+  Rect2D rect;
+  if (clipstack.empty()) {
+    rect.x1 = 0;
+    rect.y1 = 0;
+    rect.width = m_width;
+    rect.height = m_height;
+  } else {
+    rect = clipstack.back();
+  }
+  XRectangle clipwin;
+  clipwin.x = rect.x1;
+  clipwin.y = rect.y1;
+  clipwin.width = rect.width;
+  clipwin.height = rect.height;
+  XSetClipRectangles(m_display, m_gc, 0, 0, &clipwin, 1, Unsorted);  
+}
+
+void XWindow::BlitGrayscaleImage(Point2D pos, GrayscaleImage &img) {
+}
+
+void XWindow::BlitRGBImage(Point2D pos, RGBImage &img) {
+}
+
 void CheckDeleteQ() {
   // Check for windows to delete...
   for (int i=0;i<deleteQ.size();i++) {
@@ -469,6 +683,10 @@ void DoEvents() {
     p = winlist[report.xany.window];
     switch(report.type) {
     case Expose:
+      if (p->GetWindowType() == VectorWindow) {
+	XEvent repcheck;
+	while (XCheckMaskEvent(theDisplay,ExposureMask,&repcheck));
+      }
       p->OnExpose(report.xexpose.x,report.xexpose.y,
 		  report.xexpose.width,report.xexpose.height);
       break;
--- FreeMat2/src/x11/XWindow.hpp
+++ FreeMat2/src/x11/XWindow.hpp
@@ -8,9 +8,14 @@
 #include <string>
 #include "GraphicsContext.hpp"
 
-class XWindow {
+typedef enum {
+  VectorWindow,
+  BitmapWindow
+} WindowType;
+
+class XWindow : public GraphicsContext {
  public:
-  XWindow();
+  XWindow(WindowType wtype);
   virtual ~XWindow();
   Window getWindow() {return m_window;}
   int getWidth() {return m_width;}
@@ -38,7 +43,26 @@ class XWindow {
   int GetState();
   void Refresh();
   void UpdateContents(unsigned char *data, int width, int height);
+  virtual Point2D GetCanvasSize();
+  virtual Point2D GetTextExtent(std::string label);
+  virtual void DrawTextString(std::string label, Point2D pos, OrientationType orient = ORIENT_0);
+  virtual void SetFont(std::string fontname, int fontsize);
+  virtual Color SetBackGroundColor(Color col);
+  virtual Color SetForeGroundColor(Color col);
+  virtual LineStyleType SetLineStyle(LineStyleType style);
+  virtual void DrawLine(Point2D pos1, Point2D pos2);
+  virtual void DrawPoint(Point2D pos);
+  virtual void DrawCircle(Point2D pos, int radius);
+  virtual void DrawRectangle(Rect2D rect);
+  virtual void FillRectangle(Rect2D rect);
+  virtual void DrawLines(std::vector<Point2D> pts);
+  virtual void PushClippingRegion(Rect2D rect);
+  virtual Rect2D PopClippingRegion();
+  virtual void BlitGrayscaleImage(Point2D pos, GrayscaleImage &img);
+  virtual void BlitRGBImage(Point2D pos, RGBImage &img);
+  WindowType GetWindowType() {return m_type;}
  private:
+  WindowType m_type;
   Window m_window;
   Display *m_display;
   Visual *m_visual;
@@ -46,11 +70,17 @@ class XWindow {
   Pixmap m_pixmap;
   GC m_gc;
   XTextProperty m_window_title;
+  Color bg, fg;
+  LineStyleType ls;
   int m_width;
   int m_height;
   int m_state;
   int m_clickx, m_clicky;
   int m_box_x1, m_box_x2, m_box_y1, m_box_y2;
+  std::vector<Rect2D> clipstack;
+  std::string current_fontname;
+  int current_fontsize;
+  XFontStruct *font_info;
 };
 
 typedef void (*fdcallback) (void);

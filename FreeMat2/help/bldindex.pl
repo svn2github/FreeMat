f1d9779554f08e71f4322ebbc3f25f768a98605f
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-03-11 20:00:27 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@198 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/help/bldindex.pl
+++ FreeMat2/help/bldindex.pl
@@ -1,32 +1,43 @@
 #!/usr/bin/perl -w
 
-undef $/;
-
-if (!open(INPUT,"<main.tex")) {
-    die "Can't open main.tex...\n";
+if (!open(INPUT,"<html/index.html")) {
+    die "Can't open html/index.html for processing...\n";
 }
-$data=<INPUT>;
-close INPUT;
-# Collect the chapter names
-@chapts=($data =~ m/\\chapter\{([^\}]*)\}/gsm);
-# Write the chapter names to the main index
-if (!open(OUTPUT,">mtex/topics.mdc")) {
-    die "Can't open mtex/topics.mdc for writing...\n";
-}
-foreach $chapt (@chapts) {
-    ($chapt =~ /^(\w*)/g);
-    $modname = uc($1);
-    print OUTPUT "$modname -- $chapt\n";
-}
-close OUTPUT;
-# For each chapter, write out a TOC file
-foreach $chapt (@chapts) {
-    ($chapt =~ /^(\w*)/g);
-    $modname = uc($1);
-    print "$modname -- $chapt\n";
-    print "***************\n";
-    @sects=($data =~ m/\\chapter\{$chapt\}(.*)(\\chapt|\\end)/gsm);
-    foreach $sect (@sects) {
-	print $sect;
+# Skip the prefix
+while (defined($data=<INPUT>)  && !($data =~ /Table of Child-Links/g)) {};
+# Skip the header
+while (defined($data=<INPUT>) && !($data =~ /<UL>/g)) {};
+print $data;
+# Scan through the file..  
+while (defined($data=<INPUT>) && !($data =~ /Table of Child-Links/g)) {
+# Does this line start with a "<LI>?
+# If so, get the next line and aggregate.
+    if ($data =~ /^<LI>/g) {
+	chop ($data);
+	$nextline=<INPUT>;
+	$entry = $data . $nextline;
+# Make sure this isn't a sub-page reference.
+	if (!($entry =~ /\#SECTION/g)) {
+# Get the name of the entry
+	    ($entry =~ />([^<]*)<\/A>/g);
+	    $name = $1;
+# Get the value of the entry
+#	    ($entry =~ /<A([^E])*EHREF=\"([^\"]*)\">/g);
+	    $pst = $entry;
+	    ($pst =~ /HREF=\"([^\"]*)\"/g);
+	    $file = $1;
+	    print "<LI> <OBJECT type=\"text/sitemap\">\n";
+	    print "       <param name=\"Name\" value=\"$name\"\>\n";
+	    print "       <param name=\"Local\" value=\"html/$file\"\>\n";
+	    print "     </OBJECT>\n";
+	}
+    } else {
+	print $data;
     }
 }
+print "</BODY></HTML>\n";
+close(INPUT);
+#$linkstart = "<!--Table of Child-Links-->";
+#$linkend = "<!--End of Table of Child-Links-->";
+#$data =~ s/$linkstart([^$linkend]*)$linkend/($1)/gm;
+#print $data;

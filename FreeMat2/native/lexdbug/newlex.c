026ded9eb5efc6c6aff4f1236afc44c68f2e3f44
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-07-08 03:16:13 +0000

    new lexer under way...
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@372 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/native/lexdbug/newlex.c
+++ FreeMat2/native/lexdbug/newlex.c
@@ -3,58 +3,102 @@
  * FLEX to do what I wanted.
  */
 
-int yylex() {
-  char c;
+char buffer[4096];
+char *datap;
+int lineNumber;
 
-  c = getchar();
-  if (isNewLine(c)) {
+int match(char *str) {
+  if (strncmp(str,datap,strlen(str)) == 0) {
+    datap += strlen(str);
+    return 1;
+  }
+  return 0;
+}
+
+int isWhitespace() {
+  return (match(" ") || match("\t"));
+}
+
+int isNewline() {
+  return (match("\n") || match("\r\n"));
+}
+
+int testAlphaChar() {
+  return (isalpha(datap[0]));
+}
+
+int testAlphaNumChar() {
+  return (isalnum(datap[0]) || (datap[0] == '_'));
+}
+
+int testDigit() {
+  return (isdigit(datap[0]));
+}
+
+int currentChar() {
+  return datap[0];
+}
+
+int discardChar() {
+  datap++;
+}
+
+void yylex() {
+  int i;
+  char ident[256];
+
+  if (isNewline()) {
     lineNumber++;
-  } else if (isWhitespace(c)) {
-  } else if (isSemicolon(c)) {
-  } else if (isCommentchar(c)) {
-    while (!isNewLine(c))
-      c = getNextCharacterFromStream();
+  } else if (isWhitespace()) {
+  } else if (match(";")) {
+  } else if (match("%")) {
+    while (!isNewline())
+      discardChar();
     lineNumber++;
   } else {
-    if ((c == '=') && (peekchar() == '=')) {
-      getchar();
-      return EQ;
+    if (match(";\n") || match(";\r\n")) {
+      lineNumber++;
+      printf("Qendstatement\n");
+      return;
     }
-    if ((c == '.') && (peekchar() == '*')) {
-      getchar();
-      return DOTTIMES;
+    if (match(";")) {
+      printf("Qendstatement\n");
+      return;
     }
-    if ((c == '.') && (peekchar() == '/')) {
-      getchar();
-      return DOTRDIV;
+    if (match("\r\n") || match("\n")) {
+      lineNumber++;
+      printf("EndStatement\n");
+      return;
     }
-    if ((c == '.') && (peekchar() == '\\')) {
-      getchar();
-      return DOTLDIV;
+    if (testAlphaChar()) {
+      i=0;
+      while (testAlphaNumChar()) {
+	ident[i++] = currentChar();
+	discardChar();
+      }
+      ident[i] = '\0';
+      printf("ident %s\n",ident);
+      return;
     }
-    if ((c == '<') && (peekchar() == '=')) {
-      getchar();
-      return LE;
+    if (testDigit()) {
+      i=0;
+      while (testDigit()) {
+	ident[i++] = currentChar();
+	discardChar();
+      }
+      ident[i] = '\0';
+      printf("number %s\n",ident);
+      return;
     }
-    if ((c == '>') && (peekchar() == '=')) {
-      getchar();
-      return GE;
-    }
-    if ((c == '~') && (peekchar() == '=')) {
-      getchar();
-      return NE;
-    }
-    if ((c == '.') && (peekchar() == '^')) {
-      getchar();
-      return DOTPOWER;
-    }
-    if ((c == '.') && (peekchar() == '\'')) {
-      getchar();
-      return DOTTRANSPOSE;
-    }
-    if ((c == ';') 
-    
+    printf("token is %c\n",currentChar());
+    discardChar();
+    return;
   }
-  
-  
+}
+
+int main(int argc, char*argv[]) {
+  strcpy(buffer,argv[1]);
+  datap = buffer;
+  while (datap[0])
+    yylex();
 }

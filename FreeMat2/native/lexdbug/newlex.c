6841415ace6b0ec0c6d538fdc77e6c22e97eb90a
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-07-10 13:32:24 +0000

    The new lexer can now handle floating point values properly.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@374 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/native/lexdbug/newlex.c
+++ FreeMat2/native/lexdbug/newlex.c
@@ -23,6 +23,10 @@ int match(char *str) {
   return 0;
 }
 
+int isE(char p) {
+  return ((p == 'e') || (p == 'E'));
+}
+
 int isWhitespace() {
   return (match(" ") || match("\t"));
 }
@@ -49,7 +53,10 @@ int testNewline() {
 }
 
 int previousChar() {
-  return datap[-1];
+  if (datap == buffer)
+    return 0;
+  else
+    return datap[-1];
 }
 
 int currentChar() {
@@ -81,6 +88,118 @@ void lexString() {
   return;
 }
 
+void lexNumber() {
+  int state;
+  int cp;
+  int i;
+  char buffer[256];
+  int intonly;
+
+  // Initialize the state...
+  state = 0;
+  cp = 0;
+  intonly = 1;
+  while (state != 7) {
+    switch (state) {
+    case 0:
+      if (datap[cp] == '.') {
+	cp++;
+	state = 3;
+	intonly = 0;
+      } else if (isdigit(datap[cp])) {
+	while (isdigit(datap[cp]))
+	  cp++;
+	state = 1;
+	break;
+      } else 
+	return;
+      break;
+    case 1:
+      if (datap[cp] == '.') {
+	intonly = 0;
+	cp++;
+	state = 5;
+	break;
+      } else if (isE(datap[cp])) {
+	intonly = 0;
+	cp++;
+	state = 2;
+	break;
+      } else 
+	state = 7;
+      break;
+    case 2:
+      if ((datap[cp] == '+') || (datap[cp] == '-')) {
+	cp++;
+	state = 6;
+      } else if (isdigit(datap[cp])) {
+	state = 6;
+      } else {
+	fprintf(stderr,"malformed float\n");
+	exit(1);
+      }
+      break;
+    case 3:
+      if (isdigit(datap[cp])) {
+	while (isdigit(datap[cp]))
+	  cp++;
+      } else
+	return;
+      state = 4;
+      break;
+    case 4:
+      if (isE(datap[cp])) {
+	intonly = 0;
+	cp++;
+	state = 2;
+	break;
+      } else
+	state = 7;
+      break;
+    case 5:
+      if (isE(datap[cp])) {
+	intonly = 0;
+	cp++;
+	state = 2;
+	break;
+      } else if (isdigit(datap[cp])) {
+	while (isdigit(datap[cp]))
+	  cp++;
+	state = 4;
+	break;
+      } else {
+	state = 7;
+      }
+      break;
+    case 6:
+      if (isdigit(datap[cp])) {
+	while (isdigit(datap[cp]))
+	  cp++;
+	state = 7;
+      } else {
+	fprintf(stderr,"malformed float\n");
+	exit(1);	
+      }
+    }
+  }
+  if ((datap[cp] == 'f') ||
+      (datap[cp] == 'F')) {
+    cp++;
+    printf("  <float>");
+  } else if (!intonly || (datap[cp] == 'd') ||
+	     (datap[cp] == 'D')) {
+    cp++;
+    printf(" <double>");
+  } else 
+    printf("  <int>");
+  for (i=0;i<cp;i++) 
+    buffer[i] = datap[i];
+  for (i=0;i<cp;i++)
+    discardChar();
+  buffer[cp] = '\0';
+  printf("token %s\n",buffer);
+}
+
 /*
  * String detection is a bit tricky, I suppose....  A quote character
  * immediately following (without whitespace) a bracket or a alphanumeric
@@ -104,7 +223,6 @@ void lexScanningState() {
       return;
     }
   if (isWhitespace()) {
-    discardChar();
     return;
   }
   if (match(";\n") || match(";\r\n")) {
@@ -143,14 +261,8 @@ void lexScanningState() {
     printf("ident %s\n",ident);
     return;
   }
-  if (testDigit()) {
-    i=0;
-    while (testDigit()) {
-      ident[i++] = currentChar();
-      discardChar();
-    }
-    ident[i] = '\0';
-    printf("number %s\n",ident);
+  if (testDigit() || currentChar() == '.') {
+    lexNumber();
     return;
   }
   if ((currentChar() == '(') ||

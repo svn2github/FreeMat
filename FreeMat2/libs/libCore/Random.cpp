ba52d81a3f683c259abe573c388497bb9b96c234
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-07-24 18:31:19 +0000

    Work started in integrating ranlib to replace the existing random number generator.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@400 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libCore/Random.cpp
+++ FreeMat2/libs/libCore/Random.cpp
@@ -22,177 +22,8 @@
 #include "Exception.hpp"
 #include "Malloc.hpp"
 #include <math.h>
-/* 
-   A C-program for MT19937, with initialization improved 2002/1/26.
-   Coded by Takuji Nishimura and Makoto Matsumoto.
-
-   Before using, initialize the state by using init_genrand(seed)  
-   or init_by_array(init_key, key_length).
-
-   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
-   All rights reserved.                          
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions
-   are met:
-
-   1. Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-
-   2. Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-
-   3. The names of its contributors may not be used to endorse or promote 
-   products derived from this software without specific prior written 
-   permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-   Any feedback is very welcome.
-   http://www.math.keio.ac.jp/matumoto/emt.html
-   email: matumoto@math.keio.ac.jp
-*/
-
+#include "ranlib.h"
 #include <stdio.h>
-
-/* Period parameters */  
-#define N 624
-#define M 397
-#define MATRIX_A 0x9908b0dfUL   /* constant vector a */
-#define UPPER_MASK 0x80000000UL /* most significant w-r bits */
-#define LOWER_MASK 0x7fffffffUL /* least significant r bits */
-
-static unsigned long mt[N]; /* the array for the state vector  */
-static int mti=N+1; /* mti==N+1 means mt[N] is not initialized */
-
-/* initializes mt[N] with a seed */
-void init_genrand(unsigned long s)
-{
-  mt[0]= s & 0xffffffffUL;
-  for (mti=1; mti<N; mti++) {
-    mt[mti] = 
-      (1812433253UL * (mt[mti-1] ^ (mt[mti-1] >> 30)) + mti); 
-    /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
-    /* In the previous versions, MSBs of the seed affect   */
-    /* only MSBs of the array mt[].                        */
-    /* 2002/01/09 modified by Makoto Matsumoto             */
-    mt[mti] &= 0xffffffffUL;
-    /* for >32 bit machines */
-  }
-}
-
-/* initialize by an array with array-length */
-/* init_key is the array for initializing keys */
-/* key_length is its length */
-void init_by_array(unsigned long init_key[], unsigned long key_length)
-{
-  int i, j, k;
-  init_genrand(19650218UL);
-  i=1; j=0;
-  k = (N>key_length ? N : key_length);
-  for (; k; k--) {
-    mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1664525UL))
-      + init_key[j] + j; /* non linear */
-    mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
-    i++; j++;
-    if (i>=N) { mt[0] = mt[N-1]; i=1; }
-    if (j>=key_length) j=0;
-  }
-  for (k=N-1; k; k--) {
-    mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 30)) * 1566083941UL))
-      - i; /* non linear */
-    mt[i] &= 0xffffffffUL; /* for WORDSIZE > 32 machines */
-    i++;
-    if (i>=N) { mt[0] = mt[N-1]; i=1; }
-  }
-
-  mt[0] = 0x80000000UL; /* MSB is 1; assuring non-zero initial array */ 
-}
-
-/* generates a random number on [0,0xffffffff]-interval */
-unsigned long genrand_int32(void)
-{
-  unsigned long y;
-  static unsigned long mag01[2]={0x0UL, MATRIX_A};
-  /* mag01[x] = x * MATRIX_A  for x=0,1 */
-
-  if (mti >= N) { /* generate N words at one time */
-    int kk;
-
-    if (mti == N+1)   /* if init_genrand() has not been called, */
-      init_genrand(5489UL); /* a default initial seed is used */
-
-    for (kk=0;kk<N-M;kk++) {
-      y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-      mt[kk] = mt[kk+M] ^ (y >> 1) ^ mag01[y & 0x1UL];
-    }
-    for (;kk<N-1;kk++) {
-      y = (mt[kk]&UPPER_MASK)|(mt[kk+1]&LOWER_MASK);
-      mt[kk] = mt[kk+(M-N)] ^ (y >> 1) ^ mag01[y & 0x1UL];
-    }
-    y = (mt[N-1]&UPPER_MASK)|(mt[0]&LOWER_MASK);
-    mt[N-1] = mt[M-1] ^ (y >> 1) ^ mag01[y & 0x1UL];
-
-    mti = 0;
-  }
-  
-  y = mt[mti++];
-
-  /* Tempering */
-  y ^= (y >> 11);
-  y ^= (y << 7) & 0x9d2c5680UL;
-  y ^= (y << 15) & 0xefc60000UL;
-  y ^= (y >> 18);
-
-  return y;
-}
-
-/* generates a random number on [0,0x7fffffff]-interval */
-long genrand_int31(void)
-{
-  return (long)(genrand_int32()>>1);
-}
-
-/* generates a random number on [0,1]-real-interval */
-double genrand_real1(void)
-{
-  return genrand_int32()*(1.0/4294967295.0); 
-  /* divided by 2^32-1 */ 
-}
-
-/* Generates a random number on [0,1)-real-interval */
-double genrand_real2(void)
-{
-  return genrand_int32()*(1.0/4294967296.0); 
-  /* divided by 2^32 */
-}
-
-/* generates a random number on (0,1)-real-interval */
-double genrand_real3(void)
-{
-  return (((double)genrand_int32()) + 0.5)*(1.0/4294967296.0); 
-  /* divided by 2^32 */
-}
-
-/* generates a random number on [0,1) with 53-bit resolution*/
-double genrand_res53(void) 
-{ 
-  unsigned long a=genrand_int32()>>5, b=genrand_int32()>>6; 
-  return(a*67108864.0+b)*(1.0/9007199254740992.0); 
-} 
-/* These real versions are due to Isaku Wada, 2002/01/09 added */
 
 namespace FreeMat { 
   static bool initialized = false;
@@ -226,6 +57,296 @@ namespace FreeMat {
     init_genrand(seedval);
     initialized = true;
     return ArrayVector();
+  }
+
+  //!
+  //@Module RANDBETA Beta Deviate Random Number Generator
+  //@@Usage
+  //Creates an array of beta random deviates based on the supplied
+  //two parameters.
+  //A beta distribution is characterized by 
+  //a density function 
+  //!
+  ArrayVector RandBeta(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 2)
+      throw Exception("randbeta requires two parameter arguments");
+    Array arg1(arg[0]);
+    Array arg2(arg[1]);
+    // Check the logic to see if one or both are scalar values
+    if (!(arg1.isScalar() || arg2.isScalar() || (arg1.getDimensions().equals(arg2.getDimensions()))))
+      throw Exception("randbeta requires either one of the two arguments to be a scalar, or both arguments to be the same size");
+    int arg1_advance;
+    int arg2_advance;
+    arg1_advance = (arg1.isScalar()) ? 0 : 1;
+    arg2_advance = (arg2.isscalar()) ? 0 : 1;
+    // Output dimension is the larger of the two
+    Dimensions outDims;
+    if (arg1.getLength() > arg2.getLength()) {
+      outDims = arg1.getDimensions();
+    } else {
+      outDims = arg2.getDimensions();
+    }
+    arg1.promoteType(FM_FLOAT_ARRAY);
+    arg2.promoteType(FM_FLOAT_ARRAY);
+    float *dp;
+    dp = (float *) Malloc(sizeof(float)*outDims.getElementCount());
+    float *p1;
+    p1 = arg1.getDataPointer();
+    float *p2;
+    p2 = arg2.getDataPointer();
+    int i;
+    for (i=0;i<outDims.getElementCount();i++) 
+      dp[i] = genbet(p1[i*arg1_advance],p2[i*arg2_advance]);
+    ArrayVector retval;
+    retval.push_back(Array(FM_FLOAT_ARRAY,outDims,dp));
+    return retval;
+  }
+  
+  //!
+  //@Module Generate Chi-Square Random Variable
+  //!
+  ArrayVector RandChi(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 1)
+      throw Exception("randchi requires exactly one parameter (the vector of degrees of freedom)");
+    Array arg1(arg[0]);
+    arg1.promoteType(FM_FLOAT_ARRAY);
+    // Output dimension is the larger of the two
+    Dimensions outDims(arg1.getDimensions());
+    float *dp;
+    dp = (float *) Malloc(sizeof(float)*outDims.getElementCount());
+    float *p1;
+    p1 = arg1.getDataPointer();
+    int i;
+    for (i=0;i<outDims.getElementCount();i++) {
+      if (p1[i] <= 0)
+	throw Exception("argument to randchi must be positive");
+      dp[i] = genchi(p1[i]);
+    }
+    ArrayVector retval;
+    retval.push_back(Array(FM_FLOAT_ARRAY,outDims,dp));
+    return retval;
+  }
+
+  //!
+  //@Module Generate Exponential Random Variable
+  //!
+  ArrayVector RandExp(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 1)
+      throw Exception("randexp requires exactly one parameter (the vector of means)");
+    Array arg1(arg[0]);
+    arg1.promoteType(FM_FLOAT_ARRAY);
+    // Output dimension is the larger of the two
+    Dimensions outDims(arg1.getDimensions());
+    float *dp;
+    dp = (float *) Malloc(sizeof(float)*outDims.getElementCount());
+    float *p1;
+    p1 = arg1.getDataPointer();
+    int i;
+    for (i=0;i<outDims.getElementCount();i++) 
+      dp[i] = genexp(p1[i]);
+    ArrayVector retval;
+    retval.push_back(Array(FM_FLOAT_ARRAY,outDims,dp));
+    return retval;
+  }
+
+  //!
+  //@Module Generate F-Distributed Random Variable
+  //!
+  ArrayVector RandF(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 2)
+      throw Exception("randf requires two parameter arguments");
+    Array arg1(arg[0]);
+    Array arg2(arg[1]);
+    // Check the logic to see if one or both are scalar values
+    if (!(arg1.isScalar() || arg2.isScalar() || (arg1.getDimensions().equals(arg2.getDimensions()))))
+      throw Exception("randf requires either one of the two arguments to be a scalar, or both arguments to be the same size");
+    int arg1_advance;
+    int arg2_advance;
+    arg1_advance = (arg1.isScalar()) ? 0 : 1;
+    arg2_advance = (arg2.isscalar()) ? 0 : 1;
+    // Output dimension is the larger of the two
+    Dimensions outDims;
+    if (arg1.getLength() > arg2.getLength()) {
+      outDims = arg1.getDimensions();
+    } else {
+      outDims = arg2.getDimensions();
+    }
+    arg1.promoteType(FM_FLOAT_ARRAY);
+    arg2.promoteType(FM_FLOAT_ARRAY);
+    float *dp;
+    dp = (float *) Malloc(sizeof(float)*outDims.getElementCount());
+    float *p1;
+    p1 = arg1.getDataPointer();
+    float *p2;
+    p2 = arg2.getDataPointer();
+    int i;
+    for (i=0;i<outDims.getElementCount();i++) {
+      if ((p1[i*arg1_advance] <= 0) || (p2[i*arg2_advance]) <= 0)
+	throw Exception("randf requires positive arguments");
+      dp[i] = genf(p1[i*arg1_advance],p2[i*arg2_advance]);
+    }
+    ArrayVector retval;
+    retval.push_back(Array(FM_FLOAT_ARRAY,outDims,dp));
+    return retval;
+  }
+
+  //!
+  //@Module Generate Gamma-Distributed Random Variable
+  //!
+  ArrayVector RandGamma(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 2)
+      throw Exception("randgamma requires two parameter arguments");
+    Array arg1(arg[0]);
+    Array arg2(arg[1]);
+    // Check the logic to see if one or both are scalar values
+    if (!(arg1.isScalar() || arg2.isScalar() || (arg1.getDimensions().equals(arg2.getDimensions()))))
+      throw Exception("randgamma requires either one of the two arguments to be a scalar, or both arguments to be the same size");
+    int arg1_advance;
+    int arg2_advance;
+    arg1_advance = (arg1.isScalar()) ? 0 : 1;
+    arg2_advance = (arg2.isscalar()) ? 0 : 1;
+    // Output dimension is the larger of the two
+    Dimensions outDims;
+    if (arg1.getLength() > arg2.getLength()) {
+      outDims = arg1.getDimensions();
+    } else {
+      outDims = arg2.getDimensions();
+    }
+    arg1.promoteType(FM_FLOAT_ARRAY);
+    arg2.promoteType(FM_FLOAT_ARRAY);
+    float *dp;
+    dp = (float *) Malloc(sizeof(float)*outDims.getElementCount());
+    float *p1;
+    p1 = arg1.getDataPointer();
+    float *p2;
+    p2 = arg2.getDataPointer();
+    int i;
+    for (i=0;i<outDims.getElementCount();i++) {
+      dp[i] = gengam(p1[i*arg1_advance],p2[i*arg2_advance]);
+    }
+    ArrayVector retval;
+    retval.push_back(Array(FM_FLOAT_ARRAY,outDims,dp));
+    return retval;
+  }
+
+  //!
+  //!
+  ArrayVector RandMulti(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 2)
+      throw Exception("randmulti requires two parameter arguments");
+    Array arg1(arg[0]);
+    Array arg2(arg[1]);
+    int N = arg1.getContentsAsIntegerScalar();
+    if (N<0) 
+      throw Exception("number of events to generate for randmulti must be a nonnegative integer");
+    arg2.promoteType(FM_FLOAT_ARRAY);
+    // Verify the correctness of the probability argument
+    float *dp;
+    dp = arg2.getDataPointer();
+    float Psum = 0.0;
+    int i;
+    for (i=0;i<arg2.getLength();i++) {
+      if ((dp[i] < 0) || (dp[i] > 1)) 
+	throw Exception("probabiliy vector argument to randmulti must have all elements between 0 and 1");
+      Psum += dp[i];
+    }
+    if (Psum > 0.99999F)
+      throw Exception("sum of probabilities (effectively) greater than 1");
+    Dimensions outDims;
+    outDims = arg2.getDimensions();
+    int32 *ip = (int32*) Malloc(sizeof(int32)*arg2.getLength());
+    genmul(N,dp,ip);
+    ArrayVector retval;
+    retval.push_back(Array(FM_INT32_ARRAY,outDims,ip));
+    return retval;
+  }
+  
+  //!
+  //@Module Generate Noncentral Chi-Square Random Variable
+  //!
+  ArrayVector RandNChi(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 2)
+      throw Exception("randnchi requires two parameter arguments");
+    Array arg1(arg[0]);
+    Array arg2(arg[1]);
+    // Check the logic to see if one or both are scalar values
+    if (!(arg1.isScalar() || arg2.isScalar() || (arg1.getDimensions().equals(arg2.getDimensions()))))
+      throw Exception("randnchi requires either one of the two arguments to be a scalar, or both arguments to be the same size");
+    int arg1_advance;
+    int arg2_advance;
+    arg1_advance = (arg1.isScalar()) ? 0 : 1;
+    arg2_advance = (arg2.isscalar()) ? 0 : 1;
+    // Output dimension is the larger of the two
+    Dimensions outDims;
+    if (arg1.getLength() > arg2.getLength()) {
+      outDims = arg1.getDimensions();
+    } else {
+      outDims = arg2.getDimensions();
+    }
+    arg1.promoteType(FM_FLOAT_ARRAY);
+    arg2.promoteType(FM_FLOAT_ARRAY);
+    float *dp;
+    dp = (float *) Malloc(sizeof(float)*outDims.getElementCount());
+    float *p1;
+    p1 = arg1.getDataPointer();
+    float *p2;
+    p2 = arg2.getDataPointer();
+    int i;
+    for (i=0;i<outDims.getElementCount();i++) {
+      if (p1[i*arg1_advance] <= 1.0)
+	throw Exception("degrees of freedom argument must be > 1.0");
+      if (p2[i*arg2_advance] < 0.0)
+	throw Exception("noncentrality parameter must be positive");
+      dp[i] = gennch(p1[i*arg1_advance],p2[i*arg2_advance]);
+    }
+    ArrayVector retval;
+    retval.push_back(Array(FM_FLOAT_ARRAY,outDims,dp));
+    return retval;
+  }
+
+  //!
+  //@Module Generate Noncentral F-Distribution Random Variable
+  //!
+  ArrayVector RandNf(int nargout, const ArrayVector& arg) {
+    if (arg.size() != 3)
+      throw Exception("randnf requires three parameter arguments");
+    Array arg1(arg[0]);
+    Array arg2(arg[1]);
+    Array arg3(arg[2]);
+    // 
+    // Check the logic to see if one or both are scalar values
+    if (!(arg1.isScalar() || arg2.isScalar() || (arg1.getDimensions().equals(arg2.getDimensions()))))
+      throw Exception("randnf requires either one of the two arguments to be a scalar, or both arguments to be the same size");
+    int arg1_advance;
+    int arg2_advance;
+    arg1_advance = (arg1.isScalar()) ? 0 : 1;
+    arg2_advance = (arg2.isscalar()) ? 0 : 1;
+    // Output dimension is the larger of the two
+    Dimensions outDims;
+    if (arg1.getLength() > arg2.getLength()) {
+      outDims = arg1.getDimensions();
+    } else {
+      outDims = arg2.getDimensions();
+    }
+    arg1.promoteType(FM_FLOAT_ARRAY);
+    arg2.promoteType(FM_FLOAT_ARRAY);
+    float *dp;
+    dp = (float *) Malloc(sizeof(float)*outDims.getElementCount());
+    float *p1;
+    p1 = arg1.getDataPointer();
+    float *p2;
+    p2 = arg2.getDataPointer();
+    int i;
+    for (i=0;i<outDims.getElementCount();i++) {
+      if (p1[i*arg1_advance] <= 1.0)
+	throw Exception("degrees of freedom argument must be > 1.0");
+      if (p2[i*arg2_advance] < 0.0)
+	throw Exception("noncentrality parameter must be positive");
+      dp[i] = gennch(p1[i*arg1_advance],p2[i*arg2_advance]);
+    }
+    ArrayVector retval;
+    retval.push_back(Array(FM_FLOAT_ARRAY,outDims,dp));
+    return retval;
   }
 
   //!

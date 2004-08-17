c058804ed94b772fedb13844ae20210adfd7e6f5
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-08-17 22:14:35 +0000

    Added complementary error function.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@438 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libFN/main.c
+++ FreeMat2/libs/libFN/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double derf_(double* x);
+float erf_(float* x);
+double derfc_(double* x);
+float erfc_(float* x);
 int main(int argc, char*argv[]) {
   double f, t;
   f = 0.3123323452;
-  double derf_(double* x);
   t = derf_(&f);
   printf("erf(%.12f) = %.12f\n",f,t);
+  t = derfc_(&f);
+  printf("erfc(%.12f) = %.12f\n",f,t);
   float f2, t2;
   f2 = 0.3123323452;
-  float erf_(float* x);
   t2 = erf_(&f2);
   printf("erf(%.12f) = %.12f\n",f2,t2);
+  t2 = erfc_(&f2);
+  printf("erfc(%.12f) = %.12f\n",f2,t2);
   return 0;
 }

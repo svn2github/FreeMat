0719f97c34fa874bcbefd3d6ede6c2439c359686
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-08-18 19:44:42 +0000

    Consolidated ERF routines.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@440 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libFN/main.c
+++ FreeMat2/libs/libFN/main.c
@@ -9,6 +9,10 @@ double derfcx_(double* x);
 float erfcx_(float* x);
 double dgamma_(double* x);
 float gamma_(float* x);
+double dlgama_(double* x);
+float algama_(float* x);
+double ddaw_(double* x);
+float daw_(float* x);
 int main(int argc, char*argv[]) {
   double f, t;
   f = 0.3123323452;
@@ -20,6 +24,10 @@ int main(int argc, char*argv[]) {
   printf("erfcx(%.12f) = %.12f\n",f,t);
   t = dgamma_(&f);
   printf("gamma(%.12f) = %.12f\n",f,t);
+  t = dlgama_(&f);
+  printf("dlgama(%.12f) = %.12f\n",f,t);
+  t = ddaw_(&f);
+  printf("ddaw(%.12f) = %.12f\n",f,t);
   float f2, t2;
   f2 = 0.3123323452;
   t2 = erf_(&f2);
@@ -30,5 +38,8 @@ int main(int argc, char*argv[]) {
   printf("erfcx(%.12f) = %.12f\n",f2,t2);
   t2 = gamma_(&f2);
   printf("gamma(%.12f) = %.12f\n",f2,t2);
+  t2 = algama_(&f2);
+  t2 = daw_(&f2);
+  printf("daw(%.12f) = %.12f\n",f2,t2);
   return 0;
 }

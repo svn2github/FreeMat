3895a4e2ca36d061dffe90bd016f8300c5c4ef40
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-08-20 15:21:54 +0000

    More functions added and minpack also.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@442 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libFN/main.c
+++ FreeMat2/libs/libFN/main.c
@@ -1,45 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double derf_(double* x);
-float erf_(float* x);
-double derfc_(double* x);
-float erfc_(float* x);
-double derfcx_(double* x);
-float erfcx_(float* x);
-double dgamma_(double* x);
-float gamma_(float* x);
-double dlgama_(double* x);
-float algama_(float* x);
-double ddaw_(double* x);
-float daw_(float* x);
+#define dotestdouble(x,nm) {double x(double*g); t = x(&f); printf("%s(%.12f) = %.12f\n",nm,f,t);}
+#define dotestfloat(x,nm) {float x(float*g); t2 = x(&f2); printf("%s(%.12f) = %.12f\n",nm,f2,t2);}
+
 int main(int argc, char*argv[]) {
   double f, t;
   f = 0.3123323452;
-  t = derf_(&f);
-  printf("erf(%.12f) = %.12f\n",f,t);
-  t = derfc_(&f);
-  printf("erfc(%.12f) = %.12f\n",f,t);
-  t = derfcx_(&f);
-  printf("erfcx(%.12f) = %.12f\n",f,t);
-  t = dgamma_(&f);
-  printf("gamma(%.12f) = %.12f\n",f,t);
-  t = dlgama_(&f);
-  printf("dlgama(%.12f) = %.12f\n",f,t);
-  t = ddaw_(&f);
-  printf("ddaw(%.12f) = %.12f\n",f,t);
   float f2, t2;
   f2 = 0.3123323452;
-  t2 = erf_(&f2);
-  printf("erf(%.12f) = %.12f\n",f2,t2);
-  t2 = erfc_(&f2);
-  printf("erfc(%.12f) = %.12f\n",f2,t2);
-  t2 = erfcx_(&f2);
-  printf("erfcx(%.12f) = %.12f\n",f2,t2);
-  t2 = gamma_(&f2);
-  printf("gamma(%.12f) = %.12f\n",f2,t2);
-  t2 = algama_(&f2);
-  t2 = daw_(&f2);
-  printf("daw(%.12f) = %.12f\n",f2,t2);
+  dotestdouble(derf_,"derf");
+  dotestfloat(erf_,"erf");
+  dotestdouble(derfc_,"derfc");
+  dotestfloat(erfc_,"erfc");
+  dotestdouble(derfcx_,"derfcx");
+  dotestfloat(erfcx_,"erfcx");
+  dotestdouble(dgamma_,"dgamma");
+  dotestfloat(gamma_,"gamma");
+  dotestdouble(dlgama_,"dlgamma");
+  dotestfloat(algama_,"algamma");
+  dotestdouble(ddaw_,"ddaw");
+  dotestfloat(daw_,"daw");
+  dotestdouble(dei_,"dei");
+  dotestfloat(ei_,"ei");
+  dotestdouble(dbesi0_,"dbesi0");
+  dotestfloat(besi0_,"besi0");
+  dotestdouble(dbesi1_,"dbesi1");
+  dotestfloat(besi1_,"besi1");
+  dotestdouble(dbesei0_,"dbesei0");
+  dotestfloat(besei0_,"besei0");
+  dotestdouble(dbesei1_,"dbesei1");
+  dotestfloat(besei1_,"besei1");
+  dotestdouble(dbesj0_,"dbesj0");
+  dotestfloat(besj0_,"besj0");
+  dotestdouble(dbesy0_,"dbesy0");
+  dotestfloat(besy0_,"besy0");
+  dotestdouble(dbesj1_,"dbesj1");
+  dotestfloat(besj1_,"besj1");
+  dotestdouble(dbesy1_,"dbesy1");
+  dotestfloat(besy1_,"besy1");
+  dotestdouble(dbesk0_,"dbesk0");
+  dotestfloat(besk0_,"besk0");
+  dotestdouble(dbesek0_,"dbesek0");
+  dotestfloat(besek0_,"besek0");
+  dotestdouble(dbesek1_,"dbesek1");
+  dotestfloat(besek1_,"besek1");
+  dotestdouble(dpsi_,"dpsi");
+  dotestfloat(psi_,"psi");
   return 0;
 }

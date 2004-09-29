4fc19015f0916623daea997d36c112f661028175
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-09-29 02:20:34 +0000

    Prototype switch from win32 to fltk for main terminal code.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@538 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/src/win32/main.cpp
+++ FreeMat2/src/win32/main.cpp
@@ -1,3 +1,4 @@
+#if 0
 #include "XWindow.hpp"
 #include "WinTerminal.hpp"
 #include <stdio.h>
@@ -9,8 +10,19 @@
 #include "PathSearch.hpp"
 #include <stdlib.h>
 #include <signal.h>
+#endif
+
+#include <windows.h>
+#include "FLTKTerminal.hpp"
+#include "WalkTree.hpp"
+#include "Module.hpp"
+#include "LoadCore.hpp"
+#include "LoadFN.hpp"
+#include "GraphicsCore.hpp"
+#include "System.hpp"
 
 using namespace FreeMat;
+#define VERSION "1.08"
 
 int GetAppPath(LPTSTR pstr,int length){
  int ret,i;
@@ -26,6 +38,50 @@ int GetAppPath(LPTSTR pstr,int length){
  return lstrlen(pstr); //return length of final string
 }
 
+int main(int argc, char *argv[]) {
+  Context *context = new Context;
+  SpecialFunctionDef *sfdef = new SpecialFunctionDef;
+  sfdef->retCount = 0;
+  sfdef->argCount = 5;
+  sfdef->name = "loadlib";
+  sfdef->fptr = LoadLibFunction;
+  context->insertFunctionGlobally(sfdef);
+  
+  sfdef = new SpecialFunctionDef;
+  sfdef->retCount = 0;
+  sfdef->argCount = 5;
+  sfdef->name = "import";
+  sfdef->fptr = ImportFunction;
+  context->insertFunctionGlobally(sfdef);
+
+  LoadCoreFunctions(context);
+  LoadFNFunctions(context);
+  LoadGraphicsCoreFunctions(context);  
+  InitializeFigureSubsystem();
+  const char *envPtr;
+  envPtr = getenv("FREEMAT_PATH");
+
+  FLTKTerminalWindow *win = new FLTKTerminalWindow(400,300,"FreeMat v " VERSION);
+  win->term()->setContext(context);
+  if (envPtr)
+    win->term()->setPath(std::string(envPtr));
+  else 
+    win->term()->setPath(std::string(""));
+  WalkTree *twalk = new WalkTree(context,win->term());
+  win->term()->outputMessage(" Freemat v");
+  win->term()->outputMessage(VERSION);
+  win->term()->outputMessage("\n");
+  win->term()->outputMessage(" Copyright (c) 2002-2004 by Samit Basu\n");
+  while (twalk->getState() != FM_STATE_QUIT) {
+    if (twalk->getState() == FM_STATE_RETALL) 
+      win->term()->clearMessageContextStack();
+    twalk->resetState();
+    twalk->evalCLI();
+  }
+  return 0;
+}
+
+#if 0
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
   SetupWinTerminalClass(hInstance);
   WinTerminal term(hInstance, iCmdShow);
@@ -70,4 +126,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
   }
   return 0;
 }
-
+#endif

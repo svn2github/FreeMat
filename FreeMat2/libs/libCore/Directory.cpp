2dc2b56f38d02d3f11de80644f4cd884cd0d303c
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-10-09 02:41:28 +0000

    changed ls() documentation.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@603 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libCore/Directory.cpp
+++ FreeMat2/libs/libCore/Directory.cpp
@@ -107,15 +107,15 @@ namespace FreeMat {
   //@@Example
   //First, we use the simplest form of the @|ls| command, in which the directory name argument is given unquoted.
   //@<
-  //ls a*.m
+  //ls m*.m
   //@>
   //Next, we use the ``traditional'' form of the function call, using both the parenthesis and the quoted string.
   //@<
-  //ls('a*.m')
+  //ls('m*.m')
   //@>
   //In the third version, we use only the quoted string argument without parenthesis.  
   //@<
-  //ls 'a*.m'
+  //ls 'm*.m'
   //@>
   //!
   ArrayVector ListFilesFunction(int nargout, const ArrayVector& arg, WalkTree* eval) {

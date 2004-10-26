edcd5bec1599634830f51ab58458ef08d5bd09b7
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2004-10-26 20:59:48 +0000

    start on sparse array manipulations.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@636 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat2/native/sparse/sparse.hpp
@@ -0,0 +1,18 @@
+class SpString {
+public:
+  int len;
+  int start_row;
+  int N_idx;
+};
+
+template <class T> 
+class SpMatrix {
+  int rows;
+  int cols;
+  int num_string;
+  int num_nonzer;
+  bool complx;
+  
+  
+  
+}

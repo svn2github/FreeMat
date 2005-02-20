8610e82675da752d1df86032b411bc182f9b0d14
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2005-02-20 19:20:59 +0000

    Updated docs for all .mpp functions.  Added a few more staple funcs.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@771 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/MFiles/copper.m
+++ FreeMat2/MFiles/copper.m
@@ -1,27 +1,13 @@
-%
-% Function map = copper
-%
-%  return a copper colormap
-
-% Copyright (c) 2002, 2003 Samit Basu
-%
-% Permission is hereby granted, free of charge, to any person obtaining a 
-% copy of this software and associated documentation files (the "Software"), 
-% to deal in the Software without restriction, including without limitation 
-% the rights to use, copy, modify, merge, publish, distribute, sublicense, 
-% and/or sell copies of the Software, and to permit persons to whom the 
-% Software is furnished to do so, subject to the following conditions:
-%
-% The above copyright notice and this permission notice shall be included 
-% in all copies or substantial portions of the Software.
-%
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
-% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-% THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-% DEALINGS IN THE SOFTWARE.
+%  COPPER Copper Colormap
+%  
+%  
+%  USAGE
+%  
+%  Returns a copper colormap.  The syntax for its use is
+%  
+%     y = copper
+%  
+%  
 function map = copper
 basecolor = [184,115,51]/255.0; basecolor = basecolor/basecolor(1);
 map = linspace(0,1,256)'*basecolor;
--- FreeMat2/MFiles/gausfit.m
+++ FreeMat2/MFiles/gausfit.m
@@ -1,37 +1,75 @@
-%
-% Function [mu,sigma,dc,gain,yhat] = gausfit(t,y,w,mug,sigmag,dcg,gaing)
-%
-% Fits a Gaussian shape to the given vector using a nonlinear
-% Levenburg-Marquadt algorithm (fitfun).  The inputs are 
-%    t - the values of the independant variable (e.g., time samples)
-%    y - the values of the dependant variable (e.g., f(t))
-%    w - the weights to use in the fitting (set to ones if omitted)
-%
-% The fit is of the form:
-%    yhat = gain*exp((t-mu).^2/(2*sigma^2)) + dc
-%
-% Because the fit is nonlinear, a good initial guess is critical to
-% convergence of the solution.  Thus, you can supply initial guesses
-% for each of the parameters using the mug, sigmag, dcg, gaing arguments.
-% Any arguments not supplied are estimated using a simple algorithm.
-% In particular, the DC value is estimated by taking the minimum value
-% from the vector y.  The gain is estimated from the range of y.  
-% The mean and standard deviation are estimated using the first and 
-% second order moments of y.
-%
-% Example:
-% 
-% Suppose we want to fit a cycle of a cosine using a Gaussian shape.
-% t = linspace(-pi,pi); y = cos(t);
-% [mu,sigma,dc,gain,yhat] = gausfit(t,y)
+%  GAUSFIT Gaussian Curve Fit
+%  
+%  
+%  USAGE
+%  
+%  The gausfit routine has the following syntax
+%  
+%  /   [mu,sigma,dc,gain,yhat] = gausfit(t,y,w,mug,sigmag,dcg,gaing).
+%  
+%  where the required inputs are
+%  
+%    
+%  * t - the values of the independant variable (e.g., time samples)
+%    
+%  * y - the values of the dependant variable (e.g., f(t))
+%  
+%  The following inputs are all optional, and default values are
+%  available for each of them.
+%  
+%    
+%  * w - the weights to use in the fitting (set to ones if omitted)
+%    
+%  * mug - initial estimate of the mean
+%    
+%  * sigmag - initial estimate of the sigma (standard deviation)
+%    
+%  * dcg - initial estimate of the DC value
+%    
+%  * gaing - initial estimate of the gain
+%  
+%  The fit is of the form yhat=gain*exp((t-mu).^2/(2*sigma^2))+dc.
+%  The outputs are 
+%  
+%    
+%  * mu - the mean of the fit
+%    
+%  * sigma - the sigma of the fit
+%    
+%  * dc - the dc term of the fit
+%    
+%  * gain - the gain of the gaussian fit
+%    
+%  * yhat - the output samples (the Gaussian fits)
+%  
+%  Because the fit is nonlinear, a good initial guess is critical to
+%  convergence of the solution.  Thus, you can supply initial guesses
+%  for each of the parameters using the mug, sigmag, dcg, 
+%  gaing arguments.  Any arguments not supplied are estimated using 
+%  a simple algorithm. In particular, the DC value is estimated by 
+%  taking the minimum value  from the vector y.  The gain is 
+%  estimated from the range of y.  The mean and standard deviation 
+%  are estimated using the first and second order moments of y.
+%  This function uses fitfun.
+%  
 function [mu,sigma,dc,gain,yhat] = gausfit(t,y,w,mug,sigmag,dcg,gaing)
-if (~exist('w')) w = y*0+1; end
-if (~exist('dcg')) dcg = min(y(:)); end
+if (~exist('w')) 
+  w = y*0+1; 
+end
+if (~exist('dcg')) 
+  dcg = min(y(:)); 
+end
 ycor = y - dcg;
-if (~exist('gaing')) gaing = max(ycor); end
+if (~exist('gaing')) 
+  gaing = max(ycor); e
+nd
 ycor = ycor/gaing;
-if (~exist('mug')) mug = sum(ycor.*t)/sum(ycor); end
-if (~exist('sigmag')) sigmag = sqrt(abs(sum((ycor).*(t-mug).^2)/sum(ycor))); end
+if (~exist('mug')) 
+  mug = sum(ycor.*t)/sum(ycor); 
+end
+if (~exist('sigmag')) 
+  sigmag = sqrt(abs(sum((ycor).*(t-mug).^2)/sum(ycor))); 
+end
 [xopt,err] = fitfun('gfitfun',[mug,sigmag,dcg,gaing],y,w,eps,t);
 mu = xopt(1);
 sigma = xopt(2);

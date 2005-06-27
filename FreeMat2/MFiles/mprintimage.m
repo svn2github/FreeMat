43019e2ceed83d4888df91f7b495dc460a2e2177
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2005-06-27 03:30:31 +0000

    Revived mprintimage and mprintplot for online help
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1205 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat2/MFiles/mprintimage.m
@@ -0,0 +1,5 @@
+function mprintimage(namebase)
+epsname = sprintf('%s.eps',namebase);
+print(epsname);
+pngname = sprintf('%s.png',namebase);
+print(pngname);
---
+++ FreeMat2/MFiles/mprintplot.m
@@ -0,0 +1,5 @@
+function mprintplot(namebase)
+epsname = sprintf('%s.eps',namebase);
+print(epsname);
+pngname = sprintf('%s.png',namebase);
+print(pngname);

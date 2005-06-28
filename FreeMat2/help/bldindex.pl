a2dca2bd4ecc8c61dd380b2223d6bf723df97b56
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2005-06-28 03:52:23 +0000

    changed perl script to generate QT dcf file.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1211 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/help/bldindex.pl
+++ FreeMat2/help/bldindex.pl
@@ -1,38 +1,24 @@
 #!/usr/bin/perl -w
 
 open(INPUT,"<html/index.html") || die "Can't open html/index.html for processing...\n";
-
-open(OUTPUT,">toc.hhc") || die "Can't open output toc.hhc...\n";
-print OUTPUT "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML//EN\">\n";
-print OUTPUT "<HTML>\n";
-print OUTPUT "<HEAD>\n";
-print OUTPUT "<meta name=\"GENERATOR\" content=\"Microsoft&reg; HTML Help Workshop 4.1\">\n";
-print OUTPUT "<!-- Sitemap 1.0 -->\n";
-print OUTPUT "</HEAD><BODY>\n";
-print OUTPUT "<OBJECT type=\"text/site properties\">\n";
-print OUTPUT "	<param name=\"ImageType\" value=\"Folder\">\n";
-print OUTPUT "</OBJECT>\n";
-
-open(OUTPUT2,">freemat.hhk") || die "Can't open output freemat.hhk...\n";
-print OUTPUT2 "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML//EN\">\n";
-print OUTPUT2 "<HTML>\n";
-print OUTPUT2 "<HEAD>\n";
-print OUTPUT2 "<meta name=\"GENERATOR\" content=\"Microsoft&reg; HTML Help Workshop 4.1\">\n";
-print OUTPUT2 "<!-- Sitemap 1.0 -->\n";
-print OUTPUT2 "</HEAD><BODY>\n";
-print OUTPUT2 "<UL>\n";
-
+open(OUTPUT,">html/manual.dcf") || die "Can't open manual.dcf...\n";
+print OUTPUT "<assistantconfig version=\"3.2.0\">\n";
+print OUTPUT " <DCF ref=\"index.html\" >\n";
 
 # Skip the prefix
 while (defined($data=<INPUT>)  && !($data =~ /Table of Child-Links/g)) {};
 # Skip the header
 while (defined($data=<INPUT>) && !($data =~ /<UL>/g)) {};
-print OUTPUT $data;
-# Scan through the file..  
+#print OUTPUT "..";
+#print OUTPUT $data;
+# Scan through the file.. 
 while (defined($data=<INPUT>) && !($data =~ /Table of Child-Links/g)) {
 # Does this line start with a "<LI>?
 # If so, get the next line and aggregate.
-    if ($data =~ /^<LI>/g) {
+    if (($data =~ /^<UL>/g) || ($data =~ /^<BR>/g)) {
+	$opening = 1;
+    } else {
+	if ($data =~ /^<LI>/g) {
 	chop ($data);
 	$nextline=<INPUT>;
 	$entry = $data . $nextline;
@@ -46,28 +32,32 @@ while (defined($data=<INPUT>) && !($data =~ /Table of Child-Links/g)) {
 	    $pst = $entry;
 	    ($pst =~ /HREF=\"([^\"]*)\"/g);
 	    $file = $1;
-	    print OUTPUT "<LI> <OBJECT type=\"text/sitemap\">\n";
-	    print OUTPUT "       <param name=\"Name\" value=\"$name\"\>\n";
-	    print OUTPUT "       <param name=\"Local\" value=\"html/$file\"\>\n";
-	    print OUTPUT "     </OBJECT>\n";
+#	    print OUTPUT "name = $name ";
+#	    print OUTPUT "file = $file ";
 	    ($name =~ /^(\w*)/g);
 	    $keyword = lc($1);
-	    print OUTPUT2 "<LI> <OBJECT type=\"text/sitemape\">\n";
-	    print OUTPUT2 "       <param name=\"Name\" value=\"$keyword\">\n";
-	    print OUTPUT2 "       <param name=\"Name\" value=\"$name\"\>\n";
-	    print OUTPUT2 "       <param name=\"Local\" value=\"html/$file\"\>\n";
-	    print OUTPUT2 "     </OBJECT>\n";
+#	    print OUTPUT "name2 = $name ";
+#	    print OUTPUT "keyword = $keyword\n";
+	    print OUTPUT "<section ref=\"$file\" title=\"$name\"";
+	    if ($opening) {
+		print OUTPUT ">\n";
+	    } else {
+		print OUTPUT "/>\n";
+	    }
+	    $opening = 0;
 	}
     } else {
-	print OUTPUT $data;
+	if ($data =~ /<\/UL>/g) {
+	    print OUTPUT "</section>\n";
+	    } else {
+		print OUTPUT "-->";
+		print OUTPUT $data;
+	    }
     }
 }
-print OUTPUT "</BODY></HTML>\n";
+}
+print OUTPUT "  </DCF>\n";
+print OUTPUT "</assistantconfig>\n";
 close(INPUT);
 close(OUTPUT);
-print OUTPUT2 "</UL>\n</BODY></HTML>\n";
 
-#$linkstart = "<!--Table of Child-Links-->";
-#$linkend = "<!--End of Table of Child-Links-->";
-#$data =~ s/$linkstart([^$linkend]*)$linkend/($1)/gm;
-#print $data;

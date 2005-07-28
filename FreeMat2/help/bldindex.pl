746fdefe1f8b22bf1a1914d72711e91a0e746d5d
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2005-07-28 20:47:48 +0000

    tweaked help-gen files - still not perfect, dcf file is wrong
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1302 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/help/bldindex.pl
+++ FreeMat2/help/bldindex.pl
@@ -8,53 +8,56 @@ print OUTPUT " <DCF ref=\"index.html\" >\n";
 # Skip the prefix
 while (defined($data=<INPUT>)  && !($data =~ /Table of Child-Links/g)) {};
 # Skip the header
-while (defined($data=<INPUT>) && !($data =~ /<UL>/g)) {};
+while (defined($data=<INPUT>) && !($data =~ /^<UL>/g)) {};
 #print OUTPUT "..";
 #print OUTPUT $data;
 # Scan through the file.. 
+$depth = 0;
 while (defined($data=<INPUT>) && !($data =~ /Table of Child-Links/g)) {
-# Does this line start with a "<LI>?
-# If so, get the next line and aggregate.
-    if (($data =~ /^<UL>/g) || ($data =~ /^<BR>/g)) {
+    print "Depth $depth\n";
+    if ($data =~ /^<BR>/g) {
+	# Does this line contain a <BR>? if so, discard
+    } elsif ($data =~ /^<UL>/g) {
+	# Does this line contain a <UL>? if so, indicate that nesting is increasing
 	$opening = 1;
+	$depth = $depth + 1;
     } else {
 	if ($data =~ /^<LI>/g) {
-	chop ($data);
-	$nextline=<INPUT>;
-	$entry = $data . $nextline;
+	    chop ($data);
+	    $nextline=<INPUT>;
+	    $entry = $data . $nextline;
 # Make sure this isn't a sub-page reference.
-	if (!($entry =~ /\#SECTION/g)) {
+	    if (!($entry =~ /\#SECTION/g)) {
 # Get the name of the entry
-	    ($entry =~ />([^<]*)<\/A>/g);
-	    $name = $1;
+		($entry =~ />([^<]*)<\/A>/g);
+		$name = $1;
 # Get the value of the entry
 #	    ($entry =~ /<A([^E])*EHREF=\"([^\"]*)\">/g);
-	    $pst = $entry;
-	    ($pst =~ /HREF=\"([^\"]*)\"/g);
-	    $file = $1;
+		$pst = $entry;
+		($pst =~ /HREF=\"([^\"]*)\"/g);
+		$file = $1;
 #	    print OUTPUT "name = $name ";
 #	    print OUTPUT "file = $file ";
-	    ($name =~ /^(\w*)/g);
-	    $keyword = lc($1);
+		($name =~ /^(\w*)/g);
+#		$keyword = lc($1);
 #	    print OUTPUT "name2 = $name ";
 #	    print OUTPUT "keyword = $keyword\n";
-	    print OUTPUT "<section ref=\"$file\" title=\"$name\"";
-	    if ($opening) {
-		print OUTPUT ">\n";
-	    } else {
-		print OUTPUT "/>\n";
+		print OUTPUT "<section ref=\"$file\" title=\"$name\"";
+		if ($opening) {
+		    print OUTPUT ">\n";
+		} else {
+		    print OUTPUT "/>\n";
+		}
+		$opening = 0;
 	    }
-	    $opening = 0;
+	} else {
+	    if ($data =~ /<\/UL>/g) {
+		$data =~ s/<\/UL>/<\/section>/g;
+		print OUTPUT "$data\n";
+		$depth = $depth-1;
+	    } 
 	}
-    } else {
-	if ($data =~ /<\/UL>/g) {
-	    print OUTPUT "</section>\n";
-	    } else {
-		print OUTPUT "-->";
-		print OUTPUT $data;
-	    }
     }
-}
 }
 print OUTPUT "  </DCF>\n";
 print OUTPUT "</assistantconfig>\n";

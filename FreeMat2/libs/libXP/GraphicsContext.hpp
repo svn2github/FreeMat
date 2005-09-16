6cf4847fbd2454552c381b09c6b9de0c4fd9032b
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2005-09-16 21:01:56 +0000

    commented out unused argument vars to remove some annoying warning messages
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1372 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libXP/GraphicsContext.hpp
+++ FreeMat2/libs/libXP/GraphicsContext.hpp
@@ -39,27 +39,32 @@ typedef enum {
 class GraphicsContext {
 public:
   virtual Point2D GetCanvasSize() {return Point2D();};
-  virtual Point2D GetTextExtent(std::string label) {return Point2D();};
+  virtual Point2D GetTextExtent(std::string /*label*/) {return Point2D();};
   virtual void DrawTextStringAligned(std::string text, Point2D pos, 
 				     XALIGNTYPE xalign, 
 				     YALIGNTYPE yalign,
 				     OrientationType orient = ORIENT_0);
-  virtual void DrawTextString(std::string label, Point2D pos, OrientationType orient = ORIENT_0) {};
+  virtual void DrawTextString(std::string /*label*/, 
+			      Point2D /*pos*/, 
+			      OrientationType orient = ORIENT_0) {};
   virtual void SetFont(int fontsize) {};
   virtual Color SetBackGroundColor(Color col) {return col;};
   virtual Color SetForeGroundColor(Color col) {return col;};
   virtual LineStyleType SetLineStyle(LineStyleType style) {return style;};
-  virtual void DrawLine(Point2D pos1, Point2D pos2) {};
-  virtual void DrawPoint(Point2D pos) {};
-  virtual void DrawCircle(Point2D pos, int radius) {};
-  virtual void DrawRectangle(Rect2D rect) {};
-  virtual void FillRectangle(Rect2D rect) {};
-  virtual void FillQuad(Point2D p1, Point2D p2, Point2D p3, Point2D p4) {};
-  virtual void DrawQuad(Point2D p1, Point2D p2, Point2D p3, Point2D p4) {};
-  virtual void DrawLines(std::vector<Point2D> pts) {};
-  virtual void PushClippingRegion(Rect2D rect) {};
+  virtual void DrawLine(Point2D /*pos1*/, Point2D /*pos2*/) {};
+  virtual void DrawPoint(Point2D /*pos*/) {};
+  virtual void DrawCircle(Point2D /*pos*/, int /*radius*/) {};
+  virtual void DrawRectangle(Rect2D /*rect*/) {};
+  virtual void FillRectangle(Rect2D /*rect*/) {};
+  virtual void FillQuad(Point2D /*p1*/, Point2D /*p2*/, 
+			Point2D /*p3*/, Point2D /*p4*/) {};
+  virtual void DrawQuad(Point2D /*p1*/, Point2D /*p2*/, 
+			Point2D /*p3*/, Point2D /*p4*/) {};
+  virtual void DrawLines(std::vector<Point2D> /*pts*/) {};
+  virtual void PushClippingRegion(Rect2D /*rect*/) {};
   virtual Rect2D PopClippingRegion() {return Rect2D();};
-  virtual void BlitImage(unsigned char *data, int width, int height, int x0, int y0) {};
+  virtual void BlitImage(unsigned char */*data*/, int /*width*/, 
+			 int /*height*/, int /*x0*/, int /*y0*/) {};
 };
 
 #endif

9ba6314baf8f619db17a383cb9978007b9c3412a
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2005-09-22 21:03:04 +0000

    Rework of handle properties.  Less code, cleaner interface.  nice.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1391 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libGraphics/HandleAxis.hpp
+++ FreeMat2/libs/libGraphics/HandleAxis.hpp
@@ -6,23 +6,98 @@
 
 namespace FreeMat {
 
+  class HandleObject {
+    map<string, HandleProperty&, less<string> > m_properties;
+  public:
+    HandleObject() {}
+    virtual ~HandleObject() {}
+    void RegisterProperty(string name, HandleProperty& prop);
+    HandleProperty& LookupProperty(string name);
+  };
+
+  class HandleProperty {
+  public:
+    HandleProperty() {}
+    virtual ~HandleProperty() {}
+    virtual Array Get() = 0;
+    virtual void Set(Array) = 0;
+  };
+  
+  class HPVector : public HandleProperty {
+    vector<double> data;
+  public:
+    HPVector() {}
+    virtual ~HPVector() {}
+    virtual Array Get();
+    virtual void Set(Array);
+    vector<double> Data() {return data;}
+    void Data(vector<double> m) {data = m;}
+  };
+
+  class HPFixedVector : public HPVector {
+    unsigned m_len;
+  public:
+    HPFixedVector(unsigned len) : m_len(len) {}
+    virtual ~HPFixedVector() {}
+    virtual void Set(Array);    
+  };
+
+  class HPString : public HandleProperty {
+    string data;
+  public:
+    HPString() {}
+    virtual ~HPString() {}
+    virtual Array Get();
+    virtual void Set(Array);
+    string Data() {return data;}
+    void Data(string m) {data = m;}
+  };
+
+  class HPStringSet : public HandleProperty {
+    vector<string> data;
+  public:
+    HPStringSet() {}
+    virtual ~HPStringSet() {}
+    virtual Array Get();
+    virtual void Set(Array);
+    vector<string> Data() {return data}
+    void Data(vector<string> m) {data = m;}
+  };
+
+  class HPColor : public HPVector {
+  public:
+    HPColor() {}
+    virtual ~HPColor() {}
+    virtual void Set(Array);    
+    Array Get();    
+  };
+
+  class HPConstrainedString : public HPString {
+    vector<string> dictionary;
+  public:
+    HPConstrainedString(vector<string>);
+    virtual Array Get();
+    virtual void Set(Array);    
+  };
+
+
   //The HandleAxis class encapsulates a 3D axis object, and is
   //manipulated through the Set/Get interface.
-  class HandleAxis {
+  class HandleAxis : HandleObject {
   public:
     // These are all the properties of the axix
-    vector<double> bkcolor, xcolor, ycolor, zcolor;
-    string xlabel, ylabel, zlabel, title;
-    string gridlinestyle;
-    vector<string> zticklabel, xticklabel, yticklabel;
-    vector<double> position;
-    bool visible;
-    vector<double> xlim, ylim, zlim, clim;
-    vector<double> xtick, ytick, ztick;
-    string xlimmode, ylimmode, zlimmode;
-    string xtickmode, ytickmode, ztickmode;
-    string xticklabelmode, yticklabelmode, zticklabelmode;
-    string xdir, ydir, zdir;
+    HPColor bkcolor, xcolor, ycolor, zcolor;
+    HPString xlabel, ylabel, zlabel, title;
+    HPString gridlinestyle;
+    HPStringSet zticklabel, xticklabel, yticklabel;
+    HPFixedVector position;
+    HPBool visible;
+    HPFixedVector xlim, ylim, zlim, clim;
+    HPVector xtick, ytick, ztick;
+    HPConstrainedString xlimmode, ylimmode, zlimmode;
+    HPConstrainedString xtickmode, ytickmode, ztickmode;
+    HPConstrainedString xticklabelmode, yticklabelmode, zticklabelmode;
+    HPConstrainedString xdir, ydir, zdir;
 
     HandleAxis();
     ~HandleAxis();

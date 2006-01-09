75c2a350d6667999b309f4912bf1c6cc54c45104
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-01-09 20:55:29 +0000

    Made into full-blown routine.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1476 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/MFiles/surf.m
+++ FreeMat2/MFiles/surf.m
@@ -1,7 +1,41 @@
-function h = surf(X,Y,Z,C)
-  if (nargin == 3)
-    h = surface('xdata',X,'ydata',Y,'zdata',Z);
+function h = surf(varargin)
+  % Check for an axes handle
+  if (nargin>=2)
+    if (isnumeric(varargin{1}) & (length(varargin{1})==1) & ...
+    ishandle(varargin{1},'axes'))
+       handle = varargin{1}(1);
+       varargin(1) = [];
+       nargin = nargin - 1;
+    else   
+       handle = newplot;
+    end
+  end
+  % search for the propertyname/value pairs
+  propstart = 0;
+  if (nargin > 2)
+    propstart = nargin-1;
+    while ((propstart >= 1) & isa(varargin{propstart},'string') & ...
+    pvalid('line',varargin{propstart}))
+      propstart = propstart - 2;
+    end
+    propstart = propstart + 2;
+  end
+  propset = {};
+  if ((propstart > 0) & (propstart < nargin))
+     propset = varargin(propstart:end);
+     varargin(propstart:end) = [];
+  end
+  if (length(varargin) == 0)
+    h = surface(propset{:});
+  elseif (length(varargin) == 1)
+    h = surface('zdata',varargin{1},propset{:});
+  elseif (length(varargin) == 3)
+    h = surface('xdata',varargin{1},'ydata',varargin{2},...
+	'zdata',varargin{3},propset{:});
+  elseif (length(varargin) == 4)
+    h = surface('xdata',varargin{1},'ydata',varargin{2},...
+	'zdata',varargin{3},'cdata',varargin{4},propset{:});
   else
-    h = surface('xdata',X,'ydata',Y,'zdata',Z,'cdata',C);
+    error('Unrecognized arguments to surf command');
   end
 

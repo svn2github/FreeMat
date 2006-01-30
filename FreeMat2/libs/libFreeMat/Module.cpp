d0e4ad956beba6462d823443bab4db295eee680f
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-01-30 04:09:46 +0000

    Added mac specific build commands for import example
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1533 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libFreeMat/Module.cpp
+++ FreeMat2/libs/libFreeMat/Module.cpp
@@ -337,7 +337,8 @@ namespace FreeMat {
   //in FreeMat.  The following only works on systems using the GNU
   //C Compiler:
   //@<
-  //system('gcc -shared -fPIC -o add.so addArrays.c');
+  //if (strcmp(computer,'MAC')) system('gcc -bundle -flat_namespace -undefined suppress -o add.so addArrays.c'); end;
+  //if (~strcmp(computer,'MAC')) system('gcc -shared -fPIC -o add.so addArrays.c'); end;
   //import('add.so','addArrays','addArrays','void','int32 N, float[N] a, float[N] b, float[N] &c');
   //a = [3,2,3,1];
   //b = [5,6,0,2]; 

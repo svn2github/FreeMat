b5dc3c60174e45defc2366d424cbdf84715aa385
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-02-28 03:01:00 +0000

    Base types mapped to Qt types to improve cross-platform builds.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1658 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libFreeMat/Types.hpp
+++ FreeMat2/libs/libFreeMat/Types.hpp
@@ -22,15 +22,18 @@
 
 #include <string>
 #include <vector>
+#include <QtGlobal>
 
 namespace FreeMat {
-  typedef unsigned char  logical;
-  typedef char           int8;
-  typedef unsigned char  uint8;
-  typedef short          int16;
-  typedef unsigned short uint16;
-  typedef int            int32;
-  typedef unsigned int   uint32;
+  typedef quint8    logical;
+  typedef qint8     int8;
+  typedef quint8    uint8;
+  typedef qint16    int16;
+  typedef quint16   uint16;
+  typedef qint32    int32;
+  typedef quint32   uint32;
+  typedef qint64    int64;
+  typedef quint64   uint64;
   typedef unsigned int   indexType;
   typedef const indexType * constIndexPtr;
 

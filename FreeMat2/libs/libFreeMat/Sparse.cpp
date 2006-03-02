df466bf6a3be6bf22b0c3f73626de2e8a2dbcdc4
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-03-02 01:44:17 +0000

    Added copyright for Thomas
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1685 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libFreeMat/Sparse.cpp
+++ FreeMat2/libs/libFreeMat/Sparse.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2002-2006 Samit Basu
+ * Copyright (c) 2006 Thomas Beutlich
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

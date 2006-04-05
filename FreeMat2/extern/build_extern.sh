f49694f9b329a3336f498806b15ad365bed73f32
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-04-05 21:11:12 +0000

    added LAPACK to auto build script
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1809 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/extern/build_extern.sh
+++ FreeMat2/extern/build_extern.sh
@@ -29,7 +29,6 @@ ConfigureBuildAutoConf()
       ./configure --prefix=`pwd`/tmp $2
       make
       make install 
-      cd ..
   fi
   touch build_$1
 }
@@ -47,6 +46,17 @@ ConfigureBuildUMFPACK()
   touch build_$1
 }
 
+ConfigureBuildLAPACK()
+{
+  echo "Configuring and building $1..."
+  if [ ! -f build_$1 ]
+  then
+     cp patches/LAPACK/Makefile_freemat $1/SRC/.
+     cd $1/SRC
+     make
+  fi
+}
+
 FetchFile ftp://ftp.santafe.edu/pub/gnu ffcall-1.10.tar.gz
 FetchFile http://www.fftw.org fftw-3.1.1.tar.gz
 FetchFile http://www.cise.ufl.edu/research/sparse/umfpack/v4.1 UMFPACKv4.1.tar.gz
@@ -54,8 +64,9 @@ FetchFile http://www.netlib.org/lapack lapack.tgz
 UnpackTarball ffcall-1.10.tar.gz
 UnpackTarball fftw-3.1.1.tar.gz
 UnpackTarball UMFPACKv4.1.tar.gz
+UnpackTarball lapack.tgz
 ConfigureBuildAutoConf ffcall-1.10
 ConfigureBuildAutoConf fftw-3.1.1
 ConfigureBuildAutoConf fftw-3.1.1 --enable-single
 ConfigureBuildUMFPACK UMFPACKv4.1
-
+ConfigureBuildLAPACK LAPACK

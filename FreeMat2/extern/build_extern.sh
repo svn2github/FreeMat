a172e9f07c454b0ac5c8b9bde8e619ffaa39ae9d
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-04-06 02:35:09 +0000

    Extern build works for Linux
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@1811 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/extern/build_extern.sh
+++ FreeMat2/extern/build_extern.sh
@@ -2,75 +2,184 @@
 FetchFile()
 {
   echo "Looking for $2..."
-  if [ ! -f $2 ] 
+  if [ ! -f Packages/$2 ] 
   then
     echo "Fetching $2 from $1..."
-    curl $1/$2 -o $2 --disable-epsv
+    curl $1/$2 -o Packages/$2 --disable-epsv
   fi
-  if [ ! -f $2 ] 
+  if [ ! -f Packages/$2 ] 
   then
-    echo "ERROR: Unable to retrieve $2 from $1... please download manually and rerun"
+    echo "ERROR: Unable to retrieve $2 from $1... please download manually into the Packages directory and rerun the build script"
     exit 1
   fi
 }
 
 UnpackTarball()
 {
-  echo "Unpacking $1..."
-  tar xfz $1
+  if [ ! -d Build/$2 ]
+  then
+    echo "Unpacking $1..."
+    cd Build
+    tar xfz ../Packages/$1
+    cd ..
+  fi
+}
+
+UnpackZip()
+{
+  if [ ! -d Build/$2 ]
+  then
+    echo "Unpacking $1..."
+    cd Build
+    unzip -q ../Packages/$1
+    cd ..
+  fi
 }
 
 ConfigureBuildAutoConf()
 {
   echo "Configuring and building $1..."
-  if [ ! -f build_$1 ]
+  if [ ! -f State/build_$2 ]
   then
       cdir=`pwd`
-      cd $1
-      ./configure --prefix=$cdir/tmp $2
+      cd Build/$1
+      ./configure --prefix=$cdir/Root $3
       make
       make install
       cd $cdir
   fi
-  touch build_$1
+  if [ -f Root/lib/$4 ]
+  then
+    touch State/build_$2
+  fi
 }
 
 ConfigureBuildUMFPACK() 
 {
   echo "Configuring and building $1..."
-  if [ ! -f build_$1 ]
+  if [ ! -f State/build_$1 ]
   then
       cdir=`pwd`
-      cd $1/AMD
-      make lib
-      cd ../UMFPACK
-      make lib
+      cd Build/$1/AMD/Source
+      make
+      cp ../Lib/libamd.a $cdir/Root/lib/.
+      cd ../../UMFPACK/Source
+      make 
+      cp ../Lib/libumfpack.a $cdir/Root/lib/.
       cd $cdir
   fi
-  touch build_$1
+  if [ -f Build/$1/UMFPACK/Lib/libumfpack.a ]
+  then
+    touch State/build_$1
+  fi
 }
 
 ConfigureBuildLAPACK()
 {
   echo "Configuring and building $1..."
-  if [ ! -f build_$1 ]
+  if [ ! -f State/build_$1 ]
   then
-     cp patches/LAPACK/Makefile_freemat $1/SRC/.
-     cd $1/SRC
-     make
+     cdir=`pwd`
+     cp patches/LAPACK/Makefile_freemat Build/$1/SRC/.
+     cd Build/$1/SRC
+     make -f Makefile_freemat $2
+     cd $cdir
+     cp Build/LAPACK/liblapack.a $cdir/Root/lib/.
+  fi
+  if [ -f Build/LAPACK/liblapack.a ]
+  then
+    touch State/build_$1
   fi
 }
 
+ConfigureBuildBLAS()
+{
+  echo "Configure and building $1..."
+  if [ ! -f State/build_$1 ]
+  then
+    cdir=`pwd`
+    cp patches/BLAS/Makefile_freemat Build/LAPACK/BLAS/SRC/.
+    cd Build/LAPACK/BLAS/SRC
+    make -f Makefile_freemat $2
+    cd $cdir
+    cp Build/LAPACK/BLAS/libblas.a $cdir/Root/lib/.
+  fi
+  if [ -f Build/LAPACK/BLAS/libblas.a ]
+  then
+    touch State/build_$1
+  fi
+}
+
+ConfigureBuildARPACK()
+{
+  echo "Configuring and building $1..."
+  if [ ! -f State/build_$1 ]
+  then
+    cdir=`pwd`
+    cp patches/ARPACK/Makefile_freemat Build/$1/.
+    cp patches/ARPACK/stat.h Build/$1/SRC/.
+    cd Build/$1
+    make -f Makefile_freemat $2
+    cd $cdir
+    cp Build/ARPACK/libarpack.a $cdir/Root/lib/.
+  fi
+  if [ -f Build/ARPACK/libarpack.a ]
+  then
+    touch State/build_$1
+  fi
+}
+
+SetupDirs()
+{
+  echo "Setting up for builds..."
+  mkdir -p Packages
+  mkdir -p Build
+  mkdir -p Root
+  mkdir -p State
+}
+
+ReportStatusPackage()
+{
+  if [ -f State/build_$1 ]
+  then
+    echo "Package $1 build: ok"
+  else
+    echo "Package $1 build: failed"
+  fi
+}
+
+ReportStatus()
+{
+  ReportStatusPackage ffcall
+  ReportStatusPackage fftw
+  ReportStatusPackage fftwf
+  ReportStatusPackage UMFPACKv4.1
+  ReportStatusPackage LAPACK
+  ReportStatusPackage BLAS
+  ReportStatusPackage ARPACK
+  ReportStatusPackage matio
+}
+
+MAKEOPTS=$1
+SetupDirs
 FetchFile ftp://ftp.santafe.edu/pub/gnu ffcall-1.10.tar.gz
 FetchFile http://www.fftw.org fftw-3.1.1.tar.gz
 FetchFile http://www.cise.ufl.edu/research/sparse/umfpack/v4.1 UMFPACKv4.1.tar.gz
 FetchFile http://www.netlib.org/lapack lapack.tgz
-UnpackTarball ffcall-1.10.tar.gz
-UnpackTarball fftw-3.1.1.tar.gz
-UnpackTarball UMFPACKv4.1.tar.gz
-UnpackTarball lapack.tgz
-ConfigureBuildAutoConf ffcall-1.10
-ConfigureBuildAutoConf fftw-3.1.1
-ConfigureBuildAutoConf fftw-3.1.1 --enable-single
-ConfigureBuildUMFPACK UMFPACKv4.1
-ConfigureBuildLAPACK LAPACK
+FetchFile http://www.caam.rice.edu/software/ARPACK/SRC arpack96.tar.gz
+FetchFile http://www.mathworks.com/matlabcentral/files/8187 matio.zip
+UnpackTarball ffcall-1.10.tar.gz ffcall-1.10
+UnpackTarball fftw-3.1.1.tar.gz fftw-3.1.1
+UnpackTarball UMFPACKv4.1.tar.gz UMFPACKv4.1
+UnpackTarball lapack.tgz LAPACK
+UnpackTarball arpack96.tar.gz ARPACK
+UnpackZip matio.zip matio
+ConfigureBuildAutoConf ffcall-1.10 ffcall $MAKEOPTS libavcall.a
+ConfigureBuildAutoConf fftw-3.1.1 fftw $MAKEOPTS libfftw3.a
+ConfigureBuildAutoConf fftw-3.1.1 fftwf --enable-single libfftw3f.a
+ConfigureBuildUMFPACK UMFPACKv4.1 $MAKEOPTS
+ConfigureBuildLAPACK LAPACK $MAKEOPTS
+ConfigureBuildBLAS BLAS $MAKEOPTS
+ConfigureBuildARPACK ARPACK $MAKEOPTS
+ConfigureBuildAutoConf matio matio $MAKEOPTS libmatio.a
+ReportStatus

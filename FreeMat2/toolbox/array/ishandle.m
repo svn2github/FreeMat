d43e12f4e514d3ef2e7f56c52ff96e2eecaeaa89
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2006-09-05 01:11:52 +0000

    Made ishandle more sophisticated
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2145 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/toolbox/array/ishandle.m
+++ FreeMat2/toolbox/array/ishandle.m
@@ -15,8 +15,16 @@
 % Copyright (c) 2002-2006 Samit Basu
 
 function b = ishandle(handle,type)
-  b = logical(0);
-  try
-   b = strcmp(get(handle,'type'),type);
-  catch
-  end
+    if (strcmp(type,'figure') && (handle<=100))
+        b = logical(1);
+        return;
+    end
+    if ((handle<=100000) || (handle ~= int32(handle)))
+        b = logical(0);
+        return;
+    end
+    b = logical(0);
+    try
+        b = strcmp(get(handle,'type'),type);
+    catch
+    end

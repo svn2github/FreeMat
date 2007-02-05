b9d0c773deaf21a0c6f5584d82c55abf4949940b
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-02-05 01:45:48 +0000

    Something strange with Qt4 sockets...
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2330 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat2/native/qt/cli/cli.cpp
@@ -0,0 +1,41 @@
+#include <QtNetwork>
+
+int main(int argc, char *argv[]) {
+  for (int i=0;i<100;i++) {
+    QTcpSocket sock;
+    sock.connectToHost("127.0.0.1",6003);
+    qDebug() << "Connect";
+    if (!sock.waitForConnected(1000)) {
+      qDebug() << "Timeout on connect: " << sock.errorString();
+      exit(1);
+    }
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    quint64 magic = 12345;
+    out << magic;
+    sock.write(block);
+    qDebug() << "Write";
+    if (!sock.waitForBytesWritten(1000)) {
+      qDebug() << "TIMEOUT: " << sock.errorString();
+      exit(1);
+    }
+    while (sock.bytesAvailable() < (int)sizeof(quint64)) {
+      if (!sock.waitForReadyRead(1000)) {
+	qDebug() << "TIMEOUT: " << sock.errorString();
+	exit(1);
+      }
+    }
+    QDataStream in(&sock);
+    quint64 magic2;
+    in >> magic2;
+    qDebug() << "Read : " << magic2;
+    sock.disconnectFromHost();
+    qDebug() << "Disconnect";
+    if (sock.state() != QAbstractSocket::UnconnectedState)
+      if (!sock.waitForDisconnected(1000)) {
+	qDebug() << "TIMEOUT: " << sock.errorString();
+	exit(1);
+      }
+  }    
+  return 0;
+}
---
+++ FreeMat2/native/qt/serv/serv.cpp
@@ -0,0 +1,43 @@
+#include <QtNetwork>
+
+int main(int argc, char *argv[]) {
+  QTcpServer server;
+  server.listen(QHostAddress::Any,6003);
+  while (1) {
+    qDebug() << "Waiting";
+    if (!server.waitForNewConnection(30000))
+      qDebug() << "Timeout!";
+    else {
+      qDebug() << "Connecting";
+      QTcpSocket *sock = server.nextPendingConnection();
+      while (sock->bytesAvailable() < (int)sizeof(quint64)) {
+	if (!sock->waitForReadyRead(1000)) {
+	  qDebug() << "TIMEOUT: " << sock->errorString();
+	  exit(1);
+	}
+      }
+      QDataStream in(sock);
+      quint64 magic;
+      in >> magic;
+      qDebug() << "Read";
+      qDebug() << "Got magic number... " << magic;
+      QByteArray block;
+      QDataStream out(&block, QIODevice::WriteOnly);
+      out << magic;
+      sock->write(block);
+      qDebug() << "Write";
+      if (!sock->waitForBytesWritten(1000)) {
+	qDebug() << "TIMEOUT: " << sock->errorString();
+	exit(1);
+      }
+      sock->disconnectFromHost();
+      qDebug() << "Disconnect";
+      if (sock->state() != QAbstractSocket::UnconnectedState)
+	if (!sock->waitForDisconnected(1000)) {
+	  qDebug() << "TIMEOUT: " << sock->errorString();
+	  exit(1);
+	}
+      delete sock;
+    }
+  }
+}

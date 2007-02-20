9321e4a8e40ef80e51725121211ea5d766cb58dd
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-02-20 04:22:56 +0000

    skeleton of .m file version of helpgen in progress.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2384 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/toolbox/help/helpgen.m
+++ FreeMat2/toolbox/help/helpgen.m
@@ -1,23 +1,218 @@
 function helpgen(source_path)
-  rmdir([source_path,'/help/html'],'s');
-  rmdir([source_path,'/help/tmp'],'s');
-  rmdir([source_path,'/help/latex'],'s');
-  rmdir([source_path,'/help/text'],'s');
-  rmdir([source_path,'/help/test'],'s');
-  rmdir([source_path,'/help/toolbox'],'s');
-  
-  mkdir([source_path,'/help/html']);
-  mkdir([source_path,'/help/tmp']);
-  mkdir([source_path,'/help/latex']);
-  mkdir([source_path,'/help/text']);
-  mkdir([source_path,'/help/test']);
-  mkdir([source_path,'/help/toolbox']);
+%  rmdir([source_path,'/help/html'],'s');
+%  rmdir([source_path,'/help/tmp'],'s');
+%  rmdir([source_path,'/help/latex'],'s');
+%  rmdir([source_path,'/help/text'],'s');
+%  rmdir([source_path,'/help/test'],'s');
+%  rmdir([source_path,'/help/toolbox'],'s');
+%  
+%  mkdir([source_path,'/help/html']);
+%  mkdir([source_path,'/help/tmp']);
+%  mkdir([source_path,'/help/latex']);
+%  mkdir([source_path,'/help/text']);
+%  mkdir([source_path,'/help/test']);
+%  mkdir([source_path,'/help/toolbox']);
 
-  helpgen_processdir([source_path,'/toolbox']);
-  helpgen_processdir([source_path,'/libs']);
-  helpgen_processdir([source_path,'/src']);
-  
-  
+%  helpgen_processfile([source_path,'/toolbox/array/all.m']);
+  helpgen_processfile([source_path,'/toolbox/general/install.m']);
+%  helpgen_processfile([source_path,'/libs/libCore/Misc.cpp']);
+
+%  helpgen_processdir([source_path,'/toolbox']);
+%  helpgen_processdir([source_path,'/libs']);
+%  helpgen_processdir([source_path,'/src']);
   
 function helpgen_processfile(filename)
+  [path,name,suffix] = fileparts(filename);
+  if (strcmp(suffix,'.cpp'))
+    comment = '//';
+  elseif (strcmp(suffix,'.m'))
+    comment = '%';
+  else
+    return;
+  end
+  prefix = ['^\s*' comment];
+  pset = get_pattern_set(prefix);
+  printf('Processing file %s...\n',filename);
+  try
+    fp = fopen(filename,'r');
+  catch
+    return;
+  end
+  while (1)
+    line = getline(fp);
+    if (feof(fp))
+      fclose(fp);
+      return;
+    end
+    if (testmatch(line,pset.docblock))
+      line = getline(fp);
+      modname = mustmatch(line,pset.modulename);
+      moddesc = mustmatch(line,pset.moduledesc);
+      line = getline(fp);
+      secname = mustmatch(line,pset.sectionname);
+      printf('module %s moddesc %ssection %s\n',modname,moddesc,secname);
+      line = getline(fp);
+      while (~feof(fp) && ~testmatch(line,pset.docblock))
+         groupname = mustmatch(line,pset.groupname);
+	 printf('group: %s\n',groupname);
+         line = getline(fp);
+	 while (~feof(fp) && ~testmatch(line,pset.groupname) ...
+	       		  && ~testmatch(line,pset.docblock))
+           if (testmatch(line,pset.execin))
+	     handle_exec(line,fp,pset);
+           elseif (testmatch(line,pset.verbatimin)) 
+	     handle_verbatim(line,fp,pset);
+           elseif (testmatch(line,pset.figure))
+	     handle_figure(line,fp,pset);
+           elseif (testmatch(line,pset.eqnin))
+	     handle_equation(line,fp,pset);
+	   elseif (testmatch(line,pset.fnin))
+             handle_filedump(line,fp,pset);
+	   elseif (testmatch(line,pset.enumeratein))
+	     handle_enumerate(line,fp,pset);
+	   elseif (testmatch(line,pset.itemizein))
+	     handle_itemize(line,fp,pset);
+	   elseif (testmatch(line,pset.ccomment))
+	     handle_output(line,fp,pset);
+           else
+	     error('Unprocessed line:%s',line);
+           end
+         end
+      end;
+    end
+  end
+
+function pset = get_pattern_set(prefix)
+  pset.docblock =     [prefix '!'];
+  pset.modulename =   [prefix '@Module\s*(\w*)'];
+  pset.moduledesc =   [prefix '@Module\s*(.*)'];
+  pset.sectionname =  [prefix '@@Section\s*(\w*)'];
+  pset.groupname =    [prefix '@@(\w*)'];
+  pset.execin =       [prefix '@<(\d*)?'];
+  pset.execout =      [prefix '@>'];
+  pset.verbatimin =   [prefix '@\['];
+  pset.verbatimout =  [prefix '@\]'];
+  pset.ccomment =     [prefix '(.*)'];
+  pset.figure =       [prefix '@figure\s*(\w+)'];
+  pset.eqnin =        [prefix '\\\['];
+  pset.eqnout =       [prefix '\\\]'];
+  pset.fnin =         [prefix '@{\s*(\S+)'];
+  pset.fnout =        [prefix '@}'];
+  pset.enumeratein =  [prefix '\\begin{enumerate}'];
+  pset.enumerateout = [prefix '\\end{enumerate}'];
+  pset.itemizein =    [prefix '\\begin{itemize}'];
+  pset.itemizeout =   [prefix '\\end{itemize}'];
+  pset.item =         [prefix '\s*\\item(.*)'];
+
+function line = getline(fp)
+  persistent version
+  if (~exist('version'))
+    b = regexp(verstring,'v(.*)','tokens');
+    version = b{:}{1};
+  end;
+  line = strrep(fgetline(fp),'<VERSION_NUMBER>',version);
+
+function tok = mustmatch(line,pattern)
+  toks = regexpi(line,pattern,'tokens');
+  if (isempty(toks)) error(sprintf('Error on line: %s',line)); end;
+  tok = toks{1}{1};
+
+function res = testmatch(line,pattern)
+  res = ~isempty(regexp(line,pattern));
+
+function handle_filedump(&line,fp,pset)
+  fname = mustmatch(line,pset.fnin);
+  line = getline(fp);
+  fn = '';
+  while (~feof(fp) && ~testmatch(line,pset.fnout))
+    fn = [fn mustmatch(line,pset.ccomment)];
+    line = getline(fp);
+  end
+  printf('Writefile %s\n %s\n',fname,fn);
+  line = getline(fp);
+
+function handle_equation(&line,fp,pset)
+  line = getline(fp);
+  eqn = '';
+  while (~feof(fp) && ~testmatch(line,pset.eqnout))
+     eqn = [eqn,mustmatch(line,pset.ccomment),'\n'];
+	line = getline(fp);
+  end
+  printf('eqn: %s\n',eqn);
+  line = getline(fp);
+
+function handle_figure(&line,fp,pset)
+  printf('FIGURE:%s',mustmatch(line,pset.figure));
+  line = getline(fp);
+
+function handle_verbatim(&line,fp,pset)
+  line = getline(fp);
+  while (~feof(fp) && ~testmatch(line,pset.verbatimout))
+     printf('VERB:%s',mustmatch(line,pset.ccomment));
+    line = getline(fp);
+  end
+  if (feof(fp))
+     error('unmatched verbatim block detected!');
+  end
+  line = getline(fp);
+
+function handle_enumerate(&line,fp,pset)
+  line = getline(fp);
+  itemlist = {};
+  while (~feof(fp) && ~testmatch(line,pset.enumerateout))
+    item = mustmatch(line,pset.item);
+    line = getline(fp);
+    while (~testmatch(line,pset.item) && ...
+           ~testmatch(line,pset.enumerateout) && ...
+	   ~feof(fp))
+      item = [item, mustmatch(line,pset.ccomment)];
+      line = getline(fp);
+    end
+    itemlist = [itemlist,item];
+  end
+  if (feof(fp)), error('unmatched enumeration block'); end
+  line = getline(fp);
+  printf('Enumerate\n');
+  disp(itemlist);
+ 
+function handle_itemize(&line,fp,pset)
+  line = getline(fp);
+  itemlist = {};
+  while (~feof(fp) && ~testmatch(line,pset.itemizeout))
+    item = mustmatch(line,pset.item);
+    line = getline(fp);
+    while (~testmatch(line,pset.item) && ...
+           ~testmatch(line,pset.itemizeout) && ...
+	   ~feof(fp))
+      item = [item, mustmatch(line,pset.ccomment)];
+      line = getline(fp);
+    end
+    itemlist = [itemlist,item];
+  end
+  if (feof(fp)), error('unmatched enumeration block'); end
+  line = getline(fp);
+  printf('Itemize\n');
+  disp(itemlist);
+
+function handle_output(&line,fp,pset)
+  line = mustmatch(line,pset.ccomment);
+  printf('OUT:%s',line);
+  line = getline(fp);
+
+function handle_exec(&line,fp,pset)
+  line = mustmatch(line,pset.execin);
+  errors_expected = str2num(line);
+  printf('errors_expected %d\n',errors_expected);
+  cmdlist = {};
+  line = getline(fp);
+  while (~feof(fp) && ~testmatch(line,pset.execout))
+    if (testmatch(line,pset.ccomment))
+      cmdlist = [cmdlist,[mustmatch(line,pset.ccomment)]];
+    end
+    line = getline(fp);
+  end
+  printf('Command\n');
+  disp(cmdlist);
+  line = getline(fp);
+  
   

94414cd64c6d87118f272e0fb1dc39e9f07a5e9a
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-03-16 01:06:26 +0000

    Changed the implementation of feval to use overloading
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2454 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat2/toolbox/@inline/feval.m
@@ -0,0 +1,8 @@
+function varargout = feval(varargin)
+  if (~isa(varargin{1},'inline'))
+    [varargout{1:max(1,nargout)}] = builtin('feval',varargin{:});
+  else
+    t = varargin{1};
+    varargout = {inline_evaluate(t.expr,t.assign_args,varargin(2:end))};
+  end
+  

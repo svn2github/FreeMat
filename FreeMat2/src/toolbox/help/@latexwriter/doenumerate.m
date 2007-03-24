6d5397539e284835c942da3fe627d2b0ec499f00
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-03-24 18:46:56 +0000

    Fixed bug in latex output for enumerates
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2488 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/src/toolbox/help/@latexwriter/doenumerate.m
+++ FreeMat2/src/toolbox/help/@latexwriter/doenumerate.m
@@ -1,7 +1,7 @@
 function doenumerate(&p,itemlist)
   fprintf(p.myfile,'\\begin{enumerate}\n');
   for i=1:numel(itemlist)
-    fprintf(p.myfile,'\\item %s\n',itemlist{i});
+    fprintf(p.myfile,'\\item %s\n',expand_codes(itemlist{i}));
   end
   fprintf(p.myfile,'\\end{enumerate}\n');
   

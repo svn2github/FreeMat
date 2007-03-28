211367a6b71c3e88bbdcab466500b4fd49da27ae
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-03-28 01:07:03 +0000

    Updated build number for interpreter - rechecking candidate for 3.1 release
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2507 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libFreeMat/Interpreter.cpp
+++ FreeMat2/libs/libFreeMat/Interpreter.cpp
@@ -364,7 +364,7 @@ void Interpreter::doCLI() {
 }
 
 void Interpreter::sendGreeting() {
-  outputMessage(" " + getVersionString() + " (build 2468)\n");
+  outputMessage(" " + getVersionString() + " (build 2506)\n");
   outputMessage(" Copyright (c) 2002-2007 by Samit Basu\n");
   outputMessage(" Licensed under the GNU Public License (GPL)\n");
   outputMessage(" Type <help license> to find out more\n");

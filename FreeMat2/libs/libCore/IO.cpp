653d94a98415d67cb770a62d9ee7c5491e7c6659
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-05-07 02:37:26 +0000

    Added wavplay - working on wavrecord
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2606 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/libs/libCore/IO.cpp
+++ FreeMat2/libs/libCore/IO.cpp
@@ -31,6 +31,9 @@
 #include "Sparse.hpp"
 #include "Core.hpp"
 #include <QThread>
+#if HAVE_PORTAUDIO
+#include "portaudio.h"
+#endif
 
 class FilePtr {
 public:
@@ -46,20 +49,255 @@ void InitializeFileSubsystem() {
   if (init) 
     return;
   FilePtr *fptr = new FilePtr();
-    fptr->fp = stdin;
-    fptr->swapflag = false;
-    fileHandles.assignHandle(fptr);
-    fptr = new FilePtr();
-    fptr->fp = stdout;
-    fptr->swapflag = false;
-    fileHandles.assignHandle(fptr);
-    fptr = new FilePtr();
-    fptr->fp = stderr;
-    fptr->swapflag = false;
-    fileHandles.assignHandle(fptr);
-    init = true;
+  fptr->fp = stdin;
+  fptr->swapflag = false;
+  fileHandles.assignHandle(fptr);
+  fptr = new FilePtr();
+  fptr->fp = stdout;
+  fptr->swapflag = false;
+  fileHandles.assignHandle(fptr);
+  fptr = new FilePtr();
+  fptr->fp = stderr;
+  fptr->swapflag = false;
+  fileHandles.assignHandle(fptr);
+  init = true;
 }
 
+#if HAVE_PORTAUDIO
+
+const int FRAMES_PER_BUFFER = 64;
+
+class PlayBack {
+public:
+  const char *data;
+  int length;
+  int channels;
+  int framesToGo;
+  int ptr;
+  int elementSize;
+  Array x;
+};
+
+static int DoPlayBackCallback(const void *, void *output,
+			      unsigned long framesPerBuffer,
+			      const PaStreamCallbackTimeInfo*,
+			      PaStreamCallbackFlags,
+			      void *userData) {
+  PlayBack *pb = (PlayBack*) userData;
+  char *out = (char*) output;
+  const char *dp = pb->data;
+  int framesToCalc;
+  int finished = 0;
+  int i;
+  if( pb->framesToGo < framesPerBuffer )
+    {
+      framesToCalc = pb->framesToGo;
+      pb->framesToGo = 0;
+      finished = 1;
+    }
+  else
+    {
+      framesToCalc = framesPerBuffer;
+      pb->framesToGo -= framesPerBuffer;
+    }
+  
+  for( i=0; i<framesToCalc; i++ )
+    {
+      if (pb->channels == 1) {
+	for (int j=0;j<pb->elementSize;j++)
+	  *out++ = dp[pb->ptr*pb->elementSize + j];
+	pb->ptr++;
+      } else {
+	for (int j=0;j<pb->elementSize;j++) 
+	  *out++ = dp[pb->ptr*pb->elementSize + j];
+	for (int j=0;j<pb->elementSize;j++)
+	  *out++ = dp[(pb->ptr+pb->length)*pb->elementSize + j];
+	pb->ptr++;
+      }
+    }
+  /* zero remainder of final buffer */
+  for( ; i<(int)framesPerBuffer; i++ )
+    {
+      if (pb->channels == 1)
+	for (int j=0;j<pb->elementSize;j++)
+	  *out++ = 0;
+      else {
+	for (int j=0;j<pb->elementSize;j++) {
+	  *out++ = 0; /* left */
+	  *out++ = 0; /* right */
+	}
+      }
+    }
+  return finished;
+}
+
+static PaError err;
+static bool Initialized = false;
+static bool RunningStream = false;
+static PaStream *stream;
+static PlayBack *pb_obj;
+
+void PAInit() {
+  err = Pa_Initialize();
+  if (err != paNoError) 
+    throw Exception(string("An error occured while using the portaudio stream: ") + Pa_GetErrorText(err));
+}
+
+void PAShutdown() {
+  while ( ( err = Pa_IsStreamActive( stream ) ) == 1 )
+    Pa_Sleep(100);
+  err = Pa_CloseStream(stream);
+  if (err != paNoError) 
+    throw Exception(string("An error occured while using the portaudio stream: ") + Pa_GetErrorText(err));
+  Pa_Terminate();
+  delete pb_obj;
+  RunningStream = false;
+}
+
+void DoPlayBack(const void *data, int count, int channels, 
+		int elementSize, unsigned long SampleFormat,
+		int Rate, bool asyncMode, Array x) {
+  if (RunningStream) 
+    PAShutdown();
+  PAInit();
+  PaStreamParameters outputParameters;
+  outputParameters.device = Pa_GetDefaultOutputDevice();
+  outputParameters.channelCount = channels;
+  outputParameters.sampleFormat = SampleFormat;
+  outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
+  outputParameters.hostApiSpecificStreamInfo = NULL;
+  pb_obj = new PlayBack;
+  pb_obj->x = x;
+  pb_obj->data = (const char *)data;
+  pb_obj->length = count;
+  pb_obj->channels = channels;
+  pb_obj->ptr = 0;
+  pb_obj->framesToGo = count;
+  pb_obj->elementSize = elementSize;
+  err = Pa_OpenStream(&stream,
+		      NULL, /* no input */
+		      &outputParameters,
+		      Rate,
+		      FRAMES_PER_BUFFER,
+		      paNoFlag,
+		      DoPlayBackCallback,
+		      pb_obj);
+  if (err != paNoError) 
+    throw Exception(string("An error occured while using the portaudio stream: ") + Pa_GetErrorText(err));
+  err = Pa_StartStream(stream);
+  if (err != paNoError) 
+    throw Exception(string("An error occured while using the portaudio stream: ") + Pa_GetErrorText(err));
+  if (!asyncMode)
+    PAShutdown();
+  else
+    RunningStream = true;
+}
+#endif
+
+//!
+//@Module WAVPLAY
+//@@Section IO
+//@@Usage
+//Plays a linear PCM set of samples through the audio system.  This
+//function is only available if the @|portaudio| library was available
+//when FreeMat was built.  The syntax for the command is one of:
+//@[
+//   wavplay(y)
+//   wavplay(y,sampling_rate)
+//   wavplay(...,mode)
+//@]
+//where @|y| is a matrix of audio samples.  If @|y| has two columns, then
+//the audio playback is in stereo.  The @|y| input can be of types 
+//@|float, double, int32, int16, int8, uint8|.  For @|float| and 
+//@|double| types, the sample values in @|y| must be between @|-1| and
+//@|1|.  The @|sampling_rate| specifies the rate at which the data is 
+//recorded.  If not specified, the @|sampling_rate| defaults to @|11025Hz|.
+//Finally, you can specify a playback mode of @|'sync'| which is synchronous
+//playback or a playback mode of @|'async'| which is asynchronous playback.
+//For @|'sync'| playback, the wavplay function returns when the playback is
+//complete.  For @|'async'| playback, the function returns immediately (unless
+//a former playback is still issuing).
+//!
+ArrayVector WavPlayFunction(int nargout, const ArrayVector& argv) {
+#if HAVE_PORTAUDIO
+  if(argv.size() == 0)
+    throw Exception("wavplay requires at least one argument (the audio data to playback)");
+  Array y(argv[0]);
+  int SampleRate = 11025;
+  string mode = "SYNC";
+  if (argv.size() > 1)
+    SampleRate = ArrayToInt32(argv[1]);
+  if (argv.size() > 2)
+    mode = argv[2].getContentsAsStringUpper();
+  // Validate that the data is reasonable
+  if ((!y.is2D())  || (!y.isVector() && (y.columns() > 2)) || 
+      y.isReferenceType() || y.isComplex())
+    throw Exception("wavplay only supports playback of 1 or 2 channel signals, which means a 1 or 2 column matrix");
+  if (y.dataClass() == FM_DOUBLE)
+    y.promoteType(FM_FLOAT);
+  if ((y.dataClass() == FM_INT64) || (y.dataClass() == FM_UINT64))
+    throw Exception("wavplay does not support 64 bit data types.");
+  if (y.dataClass() == FM_UINT32)
+    throw Exception("wavplay does not support unsigned 32-bit data types.");
+  if (y.dataClass() == FM_UINT16)
+    throw Exception("wavplay does not support unsigned 16-bit data types.");
+  int samples;
+  int channels;
+  if (!y.isVector()) {
+    channels = y.columns();
+    samples = y.rows();
+  } else {
+    channels = 1;
+    samples = y.getLength();
+  }
+  if (y.dataClass() == FM_FLOAT)
+    DoPlayBack(y.getDataPointer(),samples,channels,sizeof(float),
+	       paFloat32,SampleRate,mode != "SYNC",y);
+  else if (y.dataClass() == FM_INT32)
+    DoPlayBack(y.getDataPointer(),samples,channels,sizeof(int32),
+	       paInt32,SampleRate,mode != "SYNC",y);
+  else if (y.dataClass() == FM_INT16)
+    DoPlayBack(y.getDataPointer(),samples,channels,sizeof(int16),
+	       paInt16,SampleRate,mode != "SYNC",y);
+  else if (y.dataClass() == FM_INT8)
+    DoPlayBack(y.getDataPointer(),samples,channels,sizeof(int8),
+	       paInt8,SampleRate,mode != "SYNC",y);
+  else if (y.dataClass() == FM_UINT8)
+    DoPlayBack(y.getDataPointer(),samples,channels,sizeof(uint8),
+	       paUInt8,SampleRate,mode != "SYNC",y);
+  else
+    throw Exception("wavplay does not support this data types.");
+#else
+  throw Exception("Audio read/write support not available.  Please build the PortAudio library and rebuild FreeMat to enable this functionality.");
+#endif
+  return ArrayVector();
+}
+
+//!
+//@Module WAVRECORD
+//@@Section IO
+//@@Usage
+//Records linear PCM sound from the audio system.  This function is
+//only available if the @|portaudio| library was available when FreeMat
+//was built.  The syntax for this command is one of:
+//@[
+//  y = wavrecord(samples,rate)
+//  y = wavrecord(...,channels)
+//  y = wavrecord(...,'datatype')
+//@]
+//where @|samples| is the number of samples to record, and @|rate| is the
+//sampling rate.  If not specified, the @|rate| defaults to @|11025Hz|.
+//If you want to record in stero, specify @|channels = 2|.  Finally, you
+//can specify the type of the recorded data (defaults to @|FM_DOUBLE|).
+//Valid choices are @|float, double, int32, int16, int8, uint8|.
+//!
+ArrayVector WavRecordFunction(int nargout, const ArrayVector& argv) {
+#if HAVE_PORTAUDIO
+#else
+  throw Exception("Audio read/write support not available.  Please build the PortAudio library and rebuild FreeMat to enable this functionality.");
+#endif
+  return ArrayVector();
+}
 
 //!
 //@Module SETPRINTLIMIT Set Limit For Printing Of Arrays

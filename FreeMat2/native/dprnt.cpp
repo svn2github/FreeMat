fdb49566e62d2df8930d1f3d104e8508c59f35f0
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-06-12 04:05:58 +0000

    Some progress on the new printing code for FreeMat.  Still not perfect, but most of the formats appear reasonable.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2692 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/native/dprnt.cpp
+++ FreeMat2/native/dprnt.cpp
@@ -96,7 +96,137 @@ void PrintFloatArray(T* t, int len) {
   }
 }
 
+template <class T>
+void PrintFloatArray(T* t, int len, int width, int decimals) {
+  for (int i=0;i<len;i++) {
+    printf(" ");
+    if (t[i] != 0) 
+      printf("%*.*f",width,decimals,t[i]);
+    else
+      printf("%*d",width,0);
+  }
+  printf("\n");
+}
+
+template <class T>
+void PrintComplexArray(T* t, int len, int width, int decimals) {
+  for (int i=0;i<len;i++) {
+    printf("  ");
+    if ((t[2*i] != 0) || (t[2*i+1] != 0)) {
+      printf("%*.*f",width,decimals,t[2*i]);
+      if (t[2*i+1] > 0)
+	printf(" +%*.*fi",width-1,decimals,t[2*i+1]);
+      else
+	printf(" -%*.*fi",width-1,decimals,-t[2*i+1]);
+    } else 
+      printf("%*d%*c",width,0,width+2,' ');
+  }
+  printf("\n");
+}
+
+template <class T>
+void fixEprint(int width, int decimals, T val) {
+  if (abs(ilogb(val))) decimals--;
+  printf("%*.*e",width,decimals,val);
+}
+
+template <class T>
+void PrintFloatArrayENotation(T* t, int len, int width, int decimals) {
+  for (int i=0;i<len;i++) {
+    printf(" ");
+    if (t[i] != 0) 
+      fixEprint(width,decimals,t[i]);
+    else
+      printf("%*d",width,0);
+  }
+  printf("\n");
+}
+
+template <class T>
+void PrintComplexArrayENotation(T* t, int len, int width, int decimals) {
+  for (int i=0;i<len;i++) {
+    printf("  ");
+    int mydec = decimals;
+    if ((t[2*i] != 0) || (t[2*i+1] != 0)) {
+      fixEprint(width,decimals,t[2*i]);
+      if (t[2*i+1] > 0)
+	fixEprint(width-1,decimals,t[2*i+1]);
+      else {
+	printf(" -");
+	fixEprint(width-1,decimals,-t[2*i+1]);
+      }
+    } else 
+      printf("%*d%*c",width,0,width+2,' ');
+  }
+  printf("\n");
+}
+
 int main(int argc, char *argv[]) {
+  {
+    float *a = new float[12];
+    a[0] = 27.2152;
+    a[1] = 31.754;
+    a[2] = -14.96534;
+    a[3] = 1.03423;
+    a[4] = 62.5754;
+    a[5] = 23.2184234;
+    a[6] = 0;
+    a[7] = 0;
+    a[8] = 15.754;
+    a[9] = -42.96534;
+    a[10] = 11.03423;
+    a[11] = 67.5754;
+    double *b = new double[12];
+    b[0] = 72.1525453423;
+    b[1] = 17.554523452345234;
+    b[2] = -49.65334534512645734;
+    b[3] = 10.34253472455386983;
+    b[4] = 6.575408097983;
+    b[5] = 22.18423480948502345;
+    b[6] = 0;
+    b[7] = 0;
+    b[8] = -4.334534512645734e+190;
+    b[9] = 10.34253472455386983;
+    b[10] = 6.575408097983;
+    b[11] = 22.18423480948502345;
+    printf("float/double short:\n");
+    PrintFloatArray(a,4,9,4);
+    PrintFloatArray(a+4,4,9,4);
+    printf("float/double short e:\n");
+    PrintFloatArrayENotation(a,4,11,4);
+    PrintFloatArrayENotation(a+4,4,11,4);
+    printf("float long:\n");
+    PrintFloatArray(a,4,11,7);
+    PrintFloatArray(a+4,4,11,7);
+    printf("float long e:\n");
+    PrintFloatArrayENotation(a,4,14,7);
+    PrintFloatArrayENotation(a+4,4,14,7);
+    printf("double long:\n");
+    PrintFloatArray(b,4,18,14);
+    PrintFloatArray(b+4,4,18,14);
+    printf("double long e:\n");
+    PrintFloatArrayENotation(b,4,21,14);
+    PrintFloatArrayENotation(b+4,4,21,14);
+    printf("complex/dcomplex short:\n");
+    PrintComplexArray(a,5,8,4);
+    PrintComplexArray(a+2,5,8,4);
+    printf("complex/dcomplex short e:\n");
+    PrintComplexArrayENotation(a,5,11,4);
+    PrintComplexArrayENotation(a+2,5,11,4);
+    printf("complex long:\n");
+    PrintComplexArray(a,5,11,7);
+    PrintComplexArray(a+2,5,11,7);
+    printf("complex long e:\n");
+    PrintComplexArrayENotation(a,4,14,7);
+    PrintComplexArrayENotation(a+2,4,14,7);
+    printf("dcomplex long:\n");
+    PrintComplexArray(b,3,18,14);
+    PrintComplexArray(b+4,3,18,14);
+    printf("dcomplex long e:\n");
+    PrintComplexArrayENotation(b,3,21,14);
+    PrintComplexArrayENotation(b+6,3,21,14);
+    return 1;
+  }
   {
     float *a = new float[20];
     float b = .000005123456789;

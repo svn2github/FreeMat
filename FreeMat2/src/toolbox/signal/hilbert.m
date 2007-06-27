f987fa8ac65f60abfff74a445ce417f0d8706cc1
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-06-27 02:02:49 +0000

    Patched help for Hilbert function.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2737 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/src/toolbox/signal/hilbert.m
+++ FreeMat2/src/toolbox/signal/hilbert.m
@@ -12,5 +12,22 @@
 %  y = hilbert(x)
 %@]
 %where @|x| is real vector or matrix.  If @|x| is a matrix, then he
-%Hilbert transform is computed along the columns of @|x|.  
+%Hilbert transform is computed along the columns of @|x|.
 %!
+function y = hilbert(x,n)
+  [t,ns] = shiftdim(x);
+  if (nargin == 1)
+    n = size(t,1);
+  end
+  f = fft(t,n,1);
+  h = zeros(n,1);
+  h(1) = 1;
+  if (rem(n,2) == 0)
+    h((n/2)+1) = 1;
+    h(2:(n/2)) = 2;
+  else
+    h(2:((n+1)/2)) = 2;
+  end
+  y = ifft(f.*repmat(h,1,size(f,2)));
+  y = shiftdim(y,-ns);
+

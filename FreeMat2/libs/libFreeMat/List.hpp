29ab6e9d83de4cf1d2208c133ed87d77b1860aaa
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-07-16 02:54:05 +0000

    Added new FMList class - contains an instance of a QList class - accelerates copies and destruction of empty lists.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2757 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat2/libs/libFreeMat/List.hpp
@@ -0,0 +1,120 @@
+#ifndef __List_hpp__
+#define __List_hpp__
+
+#include <QList>
+#include "Exception.hpp"
+
+template <class T>
+class FMList {
+  QList<T> *d;
+public:
+  FMList() : d(NULL) {
+  }
+  ~FMList() {
+    if (d) delete d;
+  }
+  inline int size() const {
+    if (d) 
+      return d->size(); 
+    else 
+      return 0;
+  }
+  inline T& operator[](int i) {
+    if (d) 
+      return (*d)[i]; 
+    else 
+      throw Exception("Illegal list access");
+  }
+  inline const T& operator[](int i) const {
+    if (d) 
+      return (*d)[i]; 
+    else
+      throw Exception("Illegal list access (const)");
+  }
+  inline bool empty() const {
+    if (d)
+      return d->empty();
+    else
+      return true;
+  }
+  inline void push_back(const T & value) {
+    if (d)
+      d->push_back(value);
+    else {
+      d = new QList<T>;
+      d->push_back(value);
+    }
+  }
+  inline void push_front(const T & value) {
+    if (d)
+      d->push_front(value);
+    else {
+      d = new QList<T>;
+      d->push_front(value);
+    }
+  }
+  inline const T& at(int i) const {
+    if (d)
+      return d->at(i);
+    else 
+      throw Exception("Illegal list access (at)");
+  }
+  inline void pop_front() {
+    if (d)
+      d->pop_front();
+    else
+      throw Exception("Illegal list access (pop_front)");
+  }
+  inline void pop_back() {
+    if (d)
+      d->pop_back();
+    else
+      throw Exception("Illegal list access (pop_back)");
+  }
+  inline T& front() {
+    if (d)
+      return d->front();
+    else
+      throw Exception("Illegal list access (front)");
+  }
+  inline const T& front() const {
+    if (d)
+      return d->front();
+    else
+      throw Exception("Illegal list access (const front)");
+  }
+  inline T& back() {
+    if (d)
+      return d->back();
+    else
+      throw Exception("Illegal list access (back)");
+  }
+  inline const T& back() const {
+    if (d)
+      return d->back();
+    else
+      throw Exception("Illegal list access (const back)");
+  }
+  inline FMList<T>& operator<<(const T & value) {
+    if (d) {
+      d->push_back(value);
+    } else {
+      d = new QList<T>;
+      d->push_back(value);
+    }
+    return *this;      
+  }
+  inline FMList<T>& operator+=(const FMList<T>& other) {
+    if (other.d) {
+      if (d) {
+	(*d) += *(other.d);
+      } else {
+	d = new QList<T>;
+	(*d) += *(other.d);
+      }
+    }
+    return *this;
+  }
+};
+
+#endif

453e337bb9bf8ca33d8cda1ff843904ee600da58
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-07-16 02:54:43 +0000

    Added hook to load prototype compile functions
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2758 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/src/MainApp.cpp
+++ FreeMat2/src/MainApp.cpp
@@ -764,6 +764,8 @@ ArrayVector ClcFunction(int nargout, const ArrayVector& arg) {
 }
 
 
+void LoadCompileFunction(Context* context);
+
 void LoadThreadFunctions(Context *context) {
   context->addSpecialFunction("threadid",ThreadIDFunction,0,1,NULL);
   context->addSpecialFunction("threadnew",ThreadNewFunction,0,1,NULL);
@@ -797,6 +799,7 @@ Context *MainApp::NewContext() {
     LoadHandleGraphicsFunctions(context);  
   }
   LoadThreadFunctions(context);
+  LoadCompileFunction(context);
   return context;
 }
 

d1b92a700f8ad63993df8641908de2b193c70a84
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2007-09-12 21:22:44 +0000

    Patched ctypewrite to use new ctype names
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@2869 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat2/src/toolbox/util/ctypewrite.m
+++ FreeMat2/src/toolbox/util/ctypewrite.m
@@ -1,22 +1,22 @@
 %!
-%@Module CSTRUCTWRITE Write a C Structure To File
+%@Module CTYPEWRITE Write a C Typedef To File
 %@@Section EXTERNAL
 %@@Usage 
-%The @|cstructwrite| function is a convenience function for
-%writing a C structure to a file.  This is generally a very
-%bad idea, as writing of C structures to files is notoriously
+%The @|ctypewrite| function is a convenience function for
+%writing a C typedef to a file.  This is generally a very
+%bad idea, as writing of C typedefs to files is notoriously
 %unportable.  Consider yourself warned.  The syntax for this
 %function is 
 %@[
-%  cstructwrite(fid,a,'typename')
+%  ctypewrite(fid,a,'typename')
 %@]
-%where @|a| is the FreeMat structure to write, @|'typename'| is
-%a string containing the name of the C structure to use when
-%writing the structure to the file (previously defined using
-%@|cstructdefine|), and @|fid| is the file handle returned
+%where @|a| is the FreeMat typedef to write, @|'typename'| is
+%a string containing the name of the C typedef to use when
+%writing the typedef to the file (previously defined using
+%@|ctypedefine|), and @|fid| is the file handle returned
 %by @|fopen|.
 %!
-function cstructwrite(fid,a,typename)
-  if (nargin < 3), error('cstructwrite requires 3 args, the fid, variable, and typename'); end;
-  fwrite(fid,cstructfreeze(a,typename));
+function ctypewrite(fid,a,typename)
+  if (nargin < 3), error('ctypewrite requires 3 args, the fid, variable, and typename'); end;
+  fwrite(fid,ctypefreeze(a,typename));
 

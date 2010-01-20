75a5c18f6dbcf9b65a2a0536bfe8ce6ba6fafab5
geneing <geneing@58e251e6-400e-0410-b350-93cf1ec51ee9>
2010-01-20 07:19:44 +0000

    Datacursor.. in progress...
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@3966 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/toolbox/graph/@datacursormanager/datacursormanager.m
+++ FreeMat/toolbox/graph/@datacursormanager/datacursormanager.m
@@ -1,70 +1,78 @@
 function c = datacursormanager( a )
-  if (nargin == 0)
-    c.c = [];
-    c = class(c,'datacursormanager');
-  elseif isa(a,'datacursormanager')
-    c = a;
-  else
-    c.c = a;
-    c = class(c,'datacursormanager');
-  end
-
-  p = hpoint
-  % Convert p to a fractional coordinate
-  s = get(gcf,'figsize'); s = s(1:2);
-  ar = get(gca,'PlotBoxAspectRatio');
-  ar = ar(1:2)./max(ar(1:2));
-  d = min(s.*ar);
-  ds = (s - d)/2;
-  p1 = p(:) ./ s(:); 
-  p = (p(:)-ds(:))./d;
-  
-  %p(2) = 1-p(2);
-  % Get the list ofchildren
-  children = get(gcf,'children');
-  % Check us agains each child
-  hit=0;
-  for i=1:numel(children)
-      position = get(children(i),'position')
-      if (hitTest(position,p1))
-          xlims = get(children(i),'xlim');
-          ylims = get(children(i),'ylim');
-		  xdir = get(children(i),'xdir');
-		  ydir = get(children(i),'ydir');
-		  if strcmp(xdir,'reverse')
-			p(1)=position(3)/2.-p(1);
-		  end
-		  if ~strcmp(ydir,'reverse')
-			p(2)=position(4)/2.-p(2);
-		  end
-          tpos(1) = xlims(1) + (p(1)-position(1))/position(3)*(xlims(2)-xlims(1));
-          tpos(2) = ylims(1) + (p(2)-position(1))/position(4)*(ylims(2)-ylims(1));
-          %tpos(1) = xlims(1) + (p(1)-position(1))*(xlims(2)-xlims(1));
-          %tpos(2) = ylims(1) + (p(2)-position(2))*(ylims(2)-ylims(1));
-		  
-		  
-		  hg = get( children(i),'children' );
-		  for i=1:length(hg)
-			if strcmp(get(hg(i),'type'),'image')
-			end
-			if strcmp(get(hg(i),'type'),'line')
-			end
-			if strcmp(get(hg(i),'type'),'surface')
-			end
-		  end
-	%[xlims(1)+p(1)*(xlims(2)-xlims(1)) ylims(1)+p(2)*(ylims(2)-ylims(1))]
-          ht=htext('string','blah','position',[tpos(1) tpos(2)], ...
-          'edgecolor',[0 0 0],'backgroundcolor',[1 1 1]);
-          %p
-		  %t = tpos
-          hit=1;
-      end
-  end
-  if hit==1
-      return
-  end
-  t = [nan,nan];
-
+    if (nargin == 0)
+        c.c = [];
+        c = class(c,'datacursormanager');
+    elseif isa(a,'datacursormanager')
+        c = a;
+    else
+        c.c = a;
+        c = class(c,'datacursormanager');
+    end
+    
+    p = hpoint
+    % Convert p to a fractional coordinate
+    s = get(gcf,'figsize'); s = s(1:2);
+    
+    if strcmp( get(gca,'PlotBoxAspectRatioMode'), 'manual') || strcmp(get(gca,'DataAspectRatioMode'),'manual')
+        ar = get(gca,'PlotBoxAspectRatio');
+        ar = ar(1:2)./max(ar(1:2));
+        d = min(s.*ar);
+        ds = (s - d)/2;
+        p1 = p(:) ./ s(:); 
+        p = (p(:)-ds(:))./d;
+        
+    else
+        p1 = p(:) ./ s(:); 
+        p = p1;
+    end
+    
+    % Get the list ofchildren
+    children = get(gcf,'children');
+    % Check us agains each child
+    hit=0;
+    for i=1:numel(children)
+        position = get(children(i),'position');
+        if (hitTest(position,p1))
+            xlims = get(children(i),'xlim');
+            ylims = get(children(i),'ylim');
+            xdir = get(children(i),'xdir');
+            ydir = get(children(i),'ydir');
+            if strcmp(xdir,'reverse')
+                p(1)=position(3)/2.-p(1);
+            end
+            if ~strcmp(ydir,'reverse')
+                p(2)=position(4)/2.-p(2);
+            end
+            tpos(1) = xlims(1) + (p(1)-position(1))/position(3)*(xlims(2)-xlims(1))
+            tpos(2) = ylims(1) + (p(2)-position(1))/position(4)*(ylims(2)-ylims(1))
+            %tpos(1) = xlims(1) + (p(1)-position(1))*(xlims(2)-xlims(1));
+            %tpos(2) = ylims(1) + (p(2)-position(2))*(ylims(2)-ylims(1));
+            
+            
+            hg = get( children(i),'children' );
+            for i=1:length(hg)
+                if strcmp(get(hg(i),'type'),'image')
+                    c = get( hg,'cdata');
+                    v = c(round(tpos(1)),round(tpos(2)));
+                end
+                if strcmp(get(hg(i),'type'),'line')
+                end
+                if strcmp(get(hg(i),'type'),'surface')
+                end
+            end
+            %[xlims(1)+p(1)*(xlims(2)-xlims(1)) ylims(1)+p(2)*(ylims(2)-ylims(1))]
+            ht=htext('string',sprintf('x: %.2g, y: %g\n val: %g',tpos(1),tpos(2),v),'position',[tpos(1) tpos(2)], ...
+            'edgecolor',[0 0 0],'backgroundcolor',[1 1 1]);
+            %p
+            %t = tpos
+            hit=1;
+        end
+    end
+    if hit==1
+        return
+    end
+    t = [nan,nan];
+    
 function b = hitTest(rect,p)
     if ((p(1) >= rect(1)) && (p(1) <= (rect(1)+rect(3))) && (p(2) >= rect(2)) && (p(2) <= (rect(2)+rect(4))))
         b = 1;

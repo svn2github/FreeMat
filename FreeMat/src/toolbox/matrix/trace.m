cdc39da6d216350d760e6459ddf08c4ec84318a7
tcyders <tcyders@58e251e6-400e-0410-b350-93cf1ec51ee9>
2010-01-25 15:41:29 +0000

    Added function trace per feature request
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@3977 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat/src/toolbox/matrix/trace.m
@@ -0,0 +1,28 @@
+%!
+%@@Module TRACE Sum Diagonal Elements of an Array
+%@@Section ARRAY
+%@@Usage
+% 
+% Returns the sum of the diagonal elements of a square matrix. The general
+% syntax for its use is
+%@[
+%   y = trace(x)
+%@]
+% where x is a square matrix.
+%!
+
+% Copyright (c) 2008 Samit Basu
+% Licensed under the GPL
+
+function ret = trace(x)
+   
+   [m n] = size(x);
+   
+   if m ~= n
+        error('Matrix must be square!\n');
+   end
+   
+   ret = sum(diag(x));
+   
+   end
+

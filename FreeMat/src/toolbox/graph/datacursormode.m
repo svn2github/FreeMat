5337122fa06681f45d2179ff7fb2955ae1427b32
geneing <geneing@58e251e6-400e-0410-b350-93cf1ec51ee9>
2010-02-07 07:08:29 +0000

    Added data cursor code to src/toolbox.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@3986 58e251e6-400e-0410-b350-93cf1ec51ee9

---
+++ FreeMat/src/toolbox/graph/datacursormode.m
@@ -0,0 +1,19 @@
+%!
+%@Module DATACURSORMODE Interactive Data Cursor
+%@@Section HANDLE
+%@@Usage
+%Toggles the data cursor which allows interactive data exploration. 
+%@[
+%   datacursormode('on')
+%@]
+%!
+
+% Copyright (c) 2010 Eugene Ingerman
+% Licensed under the GPL
+
+function varargout=datacursormode(varargin)
+    if nargin == 1
+        if strcmp(varargin{1}, 'on') 
+            varargout{1} = datacursormanager();
+        end
+    end

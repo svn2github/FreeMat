a096cd72bed3cad443aed5440e8db05a20dfe342
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2011-03-26 03:11:02 +0000

    Fix for [3069045] - persistent not working
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4122 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/libs/libFreeMat/Interpreter.cpp
+++ FreeMat/libs/libFreeMat/Interpreter.cpp
@@ -2418,7 +2418,7 @@ void Interpreter::globalStatement(const Tree & t) {
 //@{ count_calls.m
 //function count_calls
 //  persistent ccount
-//  if (~exist('ccount')) ccount = 0; end;
+//  if isempty(ccount); ccount = 0; end;
 //  ccount = ccount + 1;
 //  printf('Function has been called %d times\n',ccount);
 //@}

e40edc10027e72d2ccc3fcca4a34598800781bd1
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2011-03-29 05:04:09 +0000

    arrayfun is in progress...
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4142 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/libs/libCore/Array.cpp
+++ FreeMat/libs/libCore/Array.cpp
@@ -417,3 +417,98 @@ ArrayVector DiagFunction(int nargout, const ArrayVector& arg) {
   else
     return ArrayVector(GetDiagonal(arg[0],diagonalOrder));
 }
+
+//!
+//@Module ARRAYFUN Apply a Function To Elements of an Array
+//@@Section ARRAY
+//@@Usage
+//The @|arrayfun| function is used to apply a function handle
+//to each element of an input array (or arrays), and to collect
+//the outputs into an array.  The general syntax for its use is
+//@[
+//   y = arrayfun(fun, x)
+//@]
+//where @|x| is an N-dimensional array.  In this case, each 
+//element of the output @|y_i| is defined as @|fun(x_i)|.  You can
+//also supply multiple arguments to @|arrayfun|, provided all of the
+//arguments are the same size
+//@[
+//   y = arrayfun(fun, x, z,...)
+//@]
+//in which case each output @|y_i = fun(x_i,z_i,...)|.
+//
+//If the function returns multiple outputs, then @|arrayfun| can be
+//called with multiple outputs, in which case each output goes to
+//a separate array output
+//@[
+//   [y1,y2,...] = arrayfun(fun, x, z, ...)
+//@]
+//The assumption is that the output types for each call to @|fun| is
+//the same across the inputs.
+//
+//Finally, some hints can be provided to @|arrayfun| using th
+//@[
+//   [y1,y2,...] = arrayfun(fun, x, z, ..., 'param', value, 'param', value)
+//@]
+//where @|param| and @|value| take on the following possible values:
+//\begin{itemize}
+//  \item @|'UniformOutput'| - if the @|value| is @|true| then each output of @|fun|
+//   must be a scalar, and the outputs are concatenated into an array the same size
+//   as the input arrays.  If the @|value| is @|false| then the outputs are encapsulated
+//   into a cell array, with each entry in the cell array containing the call to 
+//   @|fun(x_i,z_i,...)|.
+//  \item @|'ErrorHandler'| - in this case @|value| is a function handle that gets called
+//  when @|fun| throws an error.  If @|'ErrorHandler'| is not specified, then @|arrayfun|
+//  allows the error to propogate (i.e., and exception is thrown).
+//\end{itemize}
+////Signature
+//sfunction arrayfun ArrayFunFunction
+//inputs varargin
+//output varargout
+//!
+#if 0
+ArrayVector ArrayFunFunction(int nargout, const ArrayVector& arg, 
+			     Interpreter*eval) {
+  if (arg.size() < 2) return ArrayVector(); // Don't bother
+  // Remove the key/value properties
+  int argcount = arg.size();
+  Array errorHandler;
+  bool uniformOutput = true; // We assume this to be the case
+  bool foundNVP = true;
+  while (foundNVP && (argcount >=2))
+    {
+      foundNVP = false;
+      if (arg[argcount-2].isString() &&
+	  (arg[argcount-2].asString() == "UniformOutput"))
+	{
+	  uniformOutput = arg[argcount-1].asInteger();
+	  argcount-=2;
+	  foundNVP = true;
+	}
+      if (arg[argcount-2].isString() &&
+	  (arg[argcount-2].asString() == "ErrorHandler"))
+	{
+	  errorHandler = arg[argcount-1];
+	  argcount-=2;
+	  foundNVP = true;
+	}
+    }
+  if (argcount < 2) return ArrayVector();
+  NTuple argdims = arg[1].dimensions();
+  for (int i=2;i<argcount;i++)
+    if (arg[i].dimensions() != argdims)
+      throw Exception("All arguments must match dimensions");
+  // Map the first argument to a function ptr
+  FuncPtr fptr = FuncPtrLookup(eval,arg[0]);
+  fptr->updateCode(eval);
+  ArrayVector outputs;
+  for (int i=0;i<argdims.count();i++)
+    {
+      ArrayVector input;
+      for (int j=2;j<argcount;j++)
+	input.
+      ArrayVector ret = fptr->evaluateFunc(eval,
+					   }
+    }
+}
+#endif

cba9b4844d8f1a152962e138a0a41639f77cd32d
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2011-07-09 16:22:59 +0000

    More updates to the test suite
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4197 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/toolbox/test/count_calls.m
+++ FreeMat/toolbox/test/count_calls.m
@@ -1,5 +1,5 @@
 function count_calls
   persistent ccount
-  if (~exist('ccount')) ccount = 0; end;
+  if isempty(ccount); ccount = 0; end;
   ccount = ccount + 1;
   printf('Function has been called %d times\n',ccount);
--- FreeMat/toolbox/test/test_evalin1.m
+++ FreeMat/toolbox/test/test_evalin1.m
@@ -5,4 +5,5 @@ end
 
 function do_test_evalin1_subfunc
    evalin('caller','test_val = true');
+   evalin('caller','test_val = true','test_val=false');
 end

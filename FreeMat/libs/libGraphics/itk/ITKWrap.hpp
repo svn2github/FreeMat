d1b8706e2841e4b1efe6ac167599936b8124c92d
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2012-01-17 17:41:58 +0000

    Partway towards working...
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4258 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/libs/libGraphics/itk/ITKWrap.hpp
+++ FreeMat/libs/libGraphics/itk/ITKWrap.hpp
@@ -7,24 +7,32 @@
 template <int dims, int dataclass, typename pixelclass>
 typename itk::Image<pixelclass, dims>::Pointer CreateITKFromArray(const Array& t)
 {
+  Array p = t;
+  p.ensureNotScalarEncoded();
   typedef itk::Image<pixelclass, dims> ITKType;
   typedef itk::ImportImageFilter<pixelclass, dims> ImageImportType;
-  ImageImportType::Pointer importer = ImageImportType::New();
-  ImageImportType::RegionType region;
+  typename ImageImportType::Pointer importer = ImageImportType::New();
+  typename ImageImportType::RegionType region;
   for (int i=0;i<dims;i++)
-    region.SetSize(i, static_cast<ImageImportType::RegionType::SizeValueType>(t.dimensions()[i]));
+    region.SetSize(i, p.dimensions()[i]);
   importer->SetRegion(region);
-  ImageType::SpacingType vol_spacing;
+  typename ITKType::SpacingType vol_spacing;
   for (int i=0;i<dims;i++)
     vol_spacing[i] = 1;
   importer->SetSpacing(vol_spacing);
-  ImageType::PointType vol_origin;
+  typename ITKType::PointType vol_origin;
   for (int i=0;i<dims;i++)
     vol_origin[i] = 0;
   importer->SetOrigin(vol_origin);
-  importer->SetImportPointer(reinterpret_cast<pixelclass*>(t.getConstVoidPointer()), t.length(), false);
+  importer->SetImportPointer(reinterpret_cast<pixelclass*>(p.constReal<pixelclass>()), p.length(), false);
   importer->Update();
   return importer->GetOutput();
+}
+
+template <int dims, int dataclass, typename pixelclass>
+Array CreateArrayFromITK(typename itk::Image<pixelclass,dims>::Pointer p)
+{
+  
 }
 
 #endif

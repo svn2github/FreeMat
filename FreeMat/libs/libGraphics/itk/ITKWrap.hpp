b8afc204eff9cd5f1216a48a92412fbaad3bd239
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2012-01-19 14:42:12 +0000

    Partial resurrection of ITKWrap.hpp :P
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4262 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/libs/libGraphics/itk/ITKWrap.hpp
+++ FreeMat/libs/libGraphics/itk/ITKWrap.hpp
@@ -2,12 +2,30 @@
 #define __ITKWrap_hpp__
 
 #include <itkImportImageFilter.h>
+#include <itkBinaryBallStructuringElement.h>
+#include <itkBinaryCrossStructuringElement.h>
 #include "Array.hpp"
 
-template <int dims, int dataclass, typename pixelclass>
-typename itk::Image<pixelclass, dims>::Pointer CreateITKFromArray(const Array& t)
+template <int dims, typename pixelclass>
+void PrintITK(typename itk::Image<pixelclass,dims>::Pointer p)
 {
-  Array p = t;
+  int rows = p->GetLargestPossibleRegion().GetSize()[0];
+  int cols = p->GetLargestPossibleRegion().GetSize()[1];
+  pixelclass* ptr = p->GetBufferPointer();
+  for (int i=0;i<rows;i++)
+    {
+      for (int j=0;j<cols;j++)
+	{
+	  std::cout << ptr[i*cols+j] << " ";
+	}
+      std::cout << "\n";
+    }
+}
+
+template <int dims, int dataclass, typename pixelclass>
+typename itk::Image<pixelclass, dims>::Pointer CreateITKFromArray(const Array& z)
+{
+  Array p = z.toClass(static_cast<DataClass>(dataclass));
   p.ensureNotScalarEncoded();
   typedef itk::Image<pixelclass, dims> ITKType;
   typedef itk::ImportImageFilter<pixelclass, dims> ImageImportType;
@@ -24,15 +42,80 @@ typename itk::Image<pixelclass, dims>::Pointer CreateITKFromArray(const Array& t
   for (int i=0;i<dims;i++)
     vol_origin[i] = 0;
   importer->SetOrigin(vol_origin);
-  importer->SetImportPointer(reinterpret_cast<pixelclass*>(p.constReal<pixelclass>()), p.length(), false);
+  importer->SetImportPointer(reinterpret_cast<pixelclass*>(p.getVoidPointer()), p.length(), false);
   importer->Update();
+  std::cout << "CIFA\n";
+  PrintITK<dims,pixelclass>(importer->GetOutput());
   return importer->GetOutput();
 }
 
 template <int dims, int dataclass, typename pixelclass>
 Array CreateArrayFromITK(typename itk::Image<pixelclass,dims>::Pointer p)
 {
-  
+  std::cout << "CAFI\n";
+  PrintITK<dims,pixelclass>(p);
+  NTuple d;
+  for (int i=0;i<dims;i++)
+    d.set(i,p->GetLargestPossibleRegion().GetSize()[i]);
+  Array ret(static_cast<DataClass>(dataclass),d);
+  memcpy(ret.getVoidPointer(),p->GetBufferPointer(),
+	 sizeof(pixelclass)*ret.length());
+  return ret;
 }
+
+#define ITKCases(func,arg)			\
+  switch(arg[0].dataClass()) {			\
+ case Int32:					\
+  {						\
+  if (arg[0].is2D())				\
+    return func<2,Int32,int32_t>(arg);	\
+  else						\
+    return func<3,Int32,int32_t>(arg);	\
+  break;					\
+  }						\
+ case Float:					\
+ {						\
+   if (arg[0].is2D())				\
+     return func<2,Float,float>(arg);	\
+   else						\
+     return func<3,Float,float>(arg);	\
+   break;					\
+ }						\
+ case Double:					\
+ {						\
+   if (arg[0].is2D())				\
+     return func<2,Double,double>(arg);	\
+   else						\
+     return func<3,Double,double>(arg);	\
+   break;					\
+ }						\
+ default:					\
+ throw Exception("Unhandle ITK volume type.");	\
+ }
+
+#define ITKCasesFloatOnly(func,arg)			\
+  switch(arg[0].dataClass()) {				\
+  case Float:						\
+    {							\
+      if (arg[0].is2D())				\
+	return func<2,Float,float>(arg);		\
+      else						\
+	return func<3,Float,float>(arg);		\
+      break;						\
+    }							\
+  case Double:						\
+    {							\
+      if (arg[0].is2D())				\
+	return func<2,Double,double>(arg);		\
+      else						\
+	return func<3,Double,double>(arg);		\
+      break;						\
+    }							\
+  default:						\
+    throw Exception("Unhandle ITK volume type (only float or double types allowed for this operation).");	\
+  }
+ 
+
+
 
 #endif

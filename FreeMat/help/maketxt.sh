a86c8cc41efd801bd1ec5c0306cad3401d687139
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2012-02-09 06:40:20 +0000

    More tweaks to the help generation.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4326 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/help/maketxt.sh
+++ FreeMat/help/maketxt.sh
@@ -3,10 +3,10 @@
 # the HTML versions.  It is not integrated into the help system
 # yet.  Ultimately, it should probably be replaced by some cleaner
 # mechanism.
-mkdir txt
+mkdir text
 for file in html/*.html; do 
     echo "Processing file...$file"; 
-    outfile=`echo $file | sed -e 's/html/txt/'`
+    outfile=`echo $file | sed -e 's/html/text/'`
     outfile=`echo $outfile | sed -e 's/\/.*_/\//'`
     outfile=`echo $outfile | sed -e 's/html/mdc/'`
     html2text -style pretty -width 60 $file | tail -n +15 > $outfile

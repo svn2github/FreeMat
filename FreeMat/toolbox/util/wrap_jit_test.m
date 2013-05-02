9001e4ef38590a80199c991ae07f81d50c28b2e5
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2013-05-02 17:07:30 +0000

    Added instrumentation for JIT tests to verify code blocks actually compiled.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4344 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/toolbox/util/wrap_jit_test.m
+++ FreeMat/toolbox/util/wrap_jit_test.m
@@ -1,17 +1,22 @@
-function wrap_jit_test(name,speedtarget)
+function wrap_jit_test(name,speedtarget,shouldjit)
 try
 jcs = jitcontrol;
 jitcontrol('off');
 tic;
 A1 = feval(name);
 base = toc;
+jitcontrol('flush')
+jcount = jitstat;
 jitcontrol('on');
 A2 = feval(name);
+jcountafter = jitstat;
 tic;
 A2 = feval(name);
 up = toc;
-printf('Speedup %f\n',base/up);
-if (issame(A1,A2) && (base/up > speedtarget))
+printf('Speedup base %f target %f up %f\n',base,speedtarget,up);
+printf('Jit succeeded %d\n',jcountafter > jcount);
+printf('Issame %d\n',issame(A1,A2));
+if (issame(A1,A2) && (base > speedtarget*up) && (shouldjit && (jcountafter > jcount)))
   saveretvalue(0);
 end
 catch

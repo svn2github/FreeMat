936faf20f298a6061d7c2244a2e3fa74f1f34780
samitbasu <samitbasu@58e251e6-400e-0410-b350-93cf1ec51ee9>
2013-07-30 02:08:51 +0000

    Added 32-bit windows cross compiling instructions.
    git-svn-id: https://svn.code.sf.net/p/freemat/code/trunk@4372 58e251e6-400e-0410-b350-93cf1ec51ee9

--- FreeMat/cmake/Toolchains/Toolchain-cross-mingw32-fedora-linux.cmake
+++ FreeMat/cmake/Toolchains/Toolchain-cross-mingw32-fedora-linux.cmake
@@ -1,14 +1,22 @@
 SET(CMAKE_SYSTEM_NAME Windows)
-SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
-SET(CMAKE_Fortran_COMPILER i686-w64-mingw32-gfortran)
-SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+SET(TOOLCHAIN_PREFIX i686-w64-mingw32)
+SET(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+SET(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+SET(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}-gfortran)
+SET(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+# Necessitated by a bug in cmake:
+# http://www.cmake.org/pipermail/cmake/2012-July/051157.html
+#
+SET(_CMAKE_TOOLCHAIN_PREFIX ${TOOLCHAIN_PREFIX}-)
 
-SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32/sys-root/mingw)
-SET(QT_MOC_EXECUTABLE /usr/i686-w64-mingw32/bin/moc)
-SET(QT_UIC_EXECUTABLE /usr/i686-w64-mingw32/bin/uic)
-SET(QT_RCC_EXECUTABLE /usr/i686-w64-mingw32/bin/rcc)
+SET(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX}/sys-root/mingw)
+SET(QT_MOC_EXECUTABLE /usr/${TOOLCHAIN_PREFIX}/bin/moc)
+SET(QT_UIC_EXECUTABLE /usr/${TOOLCHAIN_PREFIX}/bin/uic)
+SET(QT_RCC_EXECUTABLE /usr/${TOOLCHAIN_PREFIX}/bin/rcc)
 
-SET(RUNTIME_LIBS /usr/i686-w64-mingw32/sys-root/mingw/bin/libgcc_s_sjlj-1.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libstdc++-6.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/zlib1.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libpng15-15.dll /usr/i686-w64-mingw32/sys-root/mingw/bin/libsqlite3-0.dll )
+SET(RUNTIME_LIBS /usr/${TOOLCHAIN_PREFIX}/sys-root/mingw/bin/libgcc_s_sjlj-1.dll /usr/${TOOLCHAIN_PREFIX}/sys-root/mingw/bin/libstdc++-6.dll /usr/${TOOLCHAIN_PREFIX}/sys-root/mingw/bin/zlib1.dll /usr/${TOOLCHAIN_PREFIX}/sys-root/mingw/bin/libpng15-15.dll /usr/${TOOLCHAIN_PREFIX}/sys-root/mingw/bin/libsqlite3-0.dll )
 
-SET(cross_host i686-w64-mingw32)
+SET(cross_host ${TOOLCHAIN_PREFIX})
+# Necessitated by an issue in pkgconfig
+# http://www.cmake.org/pipermail/cmake/2008-July/022628.html
+SET(ENV{PKG_CONFIG_LIBDIR} ${CMAKE_FIND_ROOT_PATH}/lib/pkgconfig)
